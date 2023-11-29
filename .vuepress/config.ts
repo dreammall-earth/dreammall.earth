61e60a0614082c36e42dcf6008161d6753d6e981
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-29 10:58:50 +0100

    vuepress config

---
+++ .vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  title: 'Dreammall.earth Documentation',
+  description: 'Dreammall.earth Documentation',
+  dest: 'build/docs',
+  base: '/dreammall.earth/',
+})
