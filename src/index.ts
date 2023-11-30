e660b7fe3356d6cba4881adfcb7fa2a9292c1e7a
Moriz Wahl <moriz.wahl@gmx.de>
2023-11-30 11:17:44 +0100

    setup eslint

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
-import { ApolloServer, gql } from 'apollo-server-express'
-import express from 'express'
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
 
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`
+`;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => "Hello world!",
   },
-}
+};
 
 async function listen(port: number) {
-  const app: any = express()
-  
+  const app: any = express();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-  })
-  await server.start()
+  });
+  await server.start();
 
-  server.applyMiddleware({ app, path: '/' })
+  server.applyMiddleware({ app, path: "/" });
 
   return new Promise((resolve, reject) => {
-    app.listen(port).once('listening', resolve).once('error', reject)
-  })
+    app.listen(port).once("listening", resolve).once("error", reject);
+  });
 }
 
 async function main() {
   try {
-    await listen(4000)
-    console.log('🚀 Server is ready at http://localhost:4000/graphql')
+    await listen(4000);
+    console.log("🚀 Server is ready at http://localhost:4000/graphql");
   } catch (err) {
-    console.error('💀 Error starting the node server', err)
+    console.error("💀 Error starting the node server", err);
   }
 }
 
 void main().catch((e) => {
   // eslint-disable-next-line no-console
-  console.error(e)
-  throw e
-})
+  console.error(e);
+  throw e;
+});
