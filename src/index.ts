98896a653da447e94ad004086eee36e3ab198276
Moriz Wahl <moriz.wahl@gmx.de>
2023-11-30 11:51:56 +0100

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import { ApolloServer, gql } from "apollo-server-express";
-import express from "express";
+import { ApolloServer, gql } from 'apollo-server-express'
+import express from 'express'
 
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`;
+`
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    hello: () => 'Hello world!',
   },
-};
+}
 
 async function listen(port: number) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const app: any = express();
+  const app: any = express()
 
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-  });
-  await server.start();
+  })
+  await server.start()
 
-  server.applyMiddleware({ app, path: "/" });
+  server.applyMiddleware({ app, path: '/' })
 
-  return app.listen(port);
+  return app.listen(port)
 }
 
 async function main() {
-  await listen(4000);
+  await listen(4000)
   // eslint-disable-next-line no-console
-  console.log("🚀 Server is ready at http://localhost:4000/graphql");
+  console.log('🚀 Server is ready at http://localhost:4000/graphql')
 }
 
 main().catch((e) => {
   // eslint-disable-next-line no-console
-  console.error(e);
-  throw e;
-});
+  console.error(e)
+  throw e
+})
