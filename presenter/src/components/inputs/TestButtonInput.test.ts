bf07e9fc9a2588a3c7830a1d8cecedcca927d17d
Markus <markus@infinity.labs.ooo>
2023-12-05 12:10:48 +0100

    [feature] added tests for input components

---
+++ presenter/src/components/inputs/TestButtonInput.test.ts
@@ -0,0 +1,25 @@
+import { mount, config } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+
+import TextButtonInput from './TextButtonInput.vue'
+import MainButton from './MainButton.vue'
+
+describe('TextButtonInput', () => {
+  const wrapper = mount(TextButtonInput,{
+    props:{
+        inputLabel: "Input",
+        buttonLabel: "Button"
+    }    
+  })
+
+  it('TextButtonInput renders', () => {    
+    expect(wrapper.find('.v-field__field > .v-field__input').exists()).toBeTruthy()    
+    expect(wrapper.find('.v-field').findComponent(MainButton)).toBeTruthy()
+  })
+
+  it('TextButtonInput emit click event', () => {
+    wrapper.findComponent(MainButton).trigger('click')
+
+    expect(wrapper.findComponent(MainButton).emitted()).toHaveProperty('click')
+  })
+})
