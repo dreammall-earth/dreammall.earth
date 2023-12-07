8fa6f37f26aa16972cdee2e49cce7249a2f961e1
Markus <markus@infinity.labs.ooo>
2023-12-07 22:47:12 +0100

    [feat] added section 5 content with first slider draft

---
+++ presenter/src/components/sections/FifthSection.test.ts
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+
+import FifthSection from './FifthSection.vue'
+
+describe('renders FifthSection', () => {
+  const wrapper = mount(FifthSection)
+
+  it('renders FifthSection', () => {
+    expect(wrapper.find('.section5').exists()).toBeTruthy()
+  })
+})
