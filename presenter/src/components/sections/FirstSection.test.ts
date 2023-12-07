53c3c759f2a41e18b8113e7de17aed8d1cf8682b
Markus <markus@infinity.labs.ooo>
2023-12-07 17:29:38 +0100

    [feat] updated first section with caroussel and video

--- presenter/src/components/sections/FirstSection.test.ts
+++ presenter/src/components/sections/FirstSection.test.ts
@@ -11,20 +11,5 @@ describe('FirstSection', () => {
 
   it('renders FirstSection', () => {
     expect(wrapper.find('.section1').exists()).toBeTruthy()
-
-    expect(wrapper.find('.section1 .section-headline').exists()).toBeTruthy()
-    expect(wrapper.find('.section1 .section-headline').text()).toBe(`$t('home.section1.headline')`)
-
-    expect(wrapper.find('.section1').findComponent(LogoImage)).toBeTruthy()
-
-    expect(wrapper.find('.section1 .section-subheadline').exists()).toBeTruthy()
-    expect(wrapper.find('.section1 .section-subheadline').text()).toBe(
-      `$t('home.section1.subHeadline')`,
-    )
-
-    expect(wrapper.find('.section1').findComponent(MainButton)).toBeTruthy()
-    expect(wrapper.find('.section1').findComponent(MainButton).text()).toBe(
-      "$t('home.section1.preOrderBtn')",
-    )
   })
 })
