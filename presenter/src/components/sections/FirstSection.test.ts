449c921160aa5208a206756ebdeca5768aeee9eb
Markus <markus@infinity.labs.ooo>
2023-12-07 17:35:58 +0100

    code linting

--- presenter/src/components/sections/FirstSection.test.ts
+++ presenter/src/components/sections/FirstSection.test.ts
@@ -1,9 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 
-import MainButton from '#components/inputs/MainButton.vue'
-import LogoImage from '#components/menu/LogoImage.vue'
-
 import FirstSection from './FirstSection.vue'
 
 describe('FirstSection', () => {
