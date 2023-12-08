de8032879080a8794d4d1c0af0491bc0d7343a6b
Moriz Wahl <moriz.wahl@gmx.de>
2023-12-08 11:26:45 +0100

    include vuetify messages in locales

--- presenter/renderer/plugins/i18n.ts
+++ presenter/renderer/plugins/i18n.ts
@@ -1,14 +1,23 @@
 import { createI18n } from 'vue-i18n'
 
 import de from '#src/locales/de.json'
-// import { de as $vuetify } from 'vuetify/locale'
+import { de as vuetifyDe } from 'vuetify/locale'
 import en from '#src/locales/en.json'
-// import { en as $vuetify } from 'vuetify/locale'
+import { en as vuetifyEn } from 'vuetify/locale'
 
 export default createI18n({
   legacy: false, // Vuetify does not support the legacy mode of vue-i18n
   globalInjection: true,
   locale: 'de',
   fallbackLocale: 'en',
-  messages: { de, en },
+  messages: {
+    de: {
+      $vuetify: vuetifyDe,
+      ...de,
+    },
+    en: {
+      $vuetify: vuetifyEn,
+      ...en,
+    },
+  },
 })
