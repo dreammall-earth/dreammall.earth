8545e5fce934f71afd9ba6a7309d2421e1713995
Moriz Wahl <moriz.wahl@gmx.de>
2023-12-12 15:55:09 +0100

    input and resolver for contact form

--- backend/src/graphql/resolvers/ContactFormResolver.spec.ts
+++ backend/src/graphql/resolvers/ContactFormResolver.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ApolloServer } from '@apollo/server'
 
 import { prisma } from '#src/prisma'
@@ -9,42 +10,63 @@ beforeAll(async () => {
   testServer = await createServer()
 })
 
-describe('HelloResolver', () => {
-  describe('with no data', () => {
-    it('returns "no data found"', async () => {
-      const response = await testServer.executeOperation({
-        query: '{ hello { hello } }',
-      })
-      expect(response.body).toMatchObject({
-        kind: 'single',
-        singleResult: {
-          data: {
-            hello: {
-              hello: 'no data found',
+describe('ContactFormResolver', () => {
+  describe('createContactForm mutation', () => {
+    describe('with correct data', () => {
+      it('returns true', async () => {
+        const response = await testServer.executeOperation({
+          query: `mutation($data: ContactFormInput!) {
+                    createContactForm(contactFormData: $data) 
+                  }`,
+          variables: {
+            data: {
+              firstName: 'Peter',
+              lastName: 'Lustig',
+              acceptedDSGVO: true,
+              content: 'Hello DreamMall!',
+              email: 'peter@lustig.de',
             },
           },
-        },
+        })
+        expect(response.body).toMatchObject({
+          kind: 'single',
+          singleResult: {
+            data: {
+              createContactForm: true,
+            },
+          },
+        })
+      })
+
+      it('has the contact form stored in the database', async () => {
+        const result = await prisma.contactForm.findMany()
+        expect(result).toHaveLength(1)
+        expect(result).toEqual([
+          {
+            id: expect.any(Number),
+            firstName: 'Peter',
+            lastName: 'Lustig',
+            acceptedDSGVO: true,
+            content: 'Hello DreamMall!',
+            email: 'peter@lustig.de',
+            createdAt: expect.any(Date),
+            brevoSuccess: null,
+          },
+        ])
       })
     })
   })
 
-  describe('with data', () => {
-    it('returns "Hello World!"', async () => {
-      await prisma.hello.create({
-        data: {
-          text: 'Hello World!',
-        },
-      })
+  describe('contactForm query', () => {
+    it('returns true', async () => {
       const response = await testServer.executeOperation({
-        query: '{ hello { hello } }',
+        query: `query { contactForm }`,
       })
       expect(response.body).toMatchObject({
         kind: 'single',
         singleResult: {
           data: {
-            hello: {
-              hello: 'Hello World!',
-            },
+            contactForm: true,
           },
         },
       })
