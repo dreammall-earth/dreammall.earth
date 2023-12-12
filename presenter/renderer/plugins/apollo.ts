1ff4717a7fa6acb86c6caad7432d30f8b9dd49cd
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-12 15:01:53 +0100

    implement apollo client

---
+++ presenter/renderer/plugins/apollo.ts
@@ -0,0 +1,34 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createHttpLink } from '@apollo/client/link/http/http.cjs'
+// import { onError } from '@apollo/client/link/error'
+
+import { ENDPOINTS } from '#src/env'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: ENDPOINTS.GRAPHQL_URI,
+})
+
+/*
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    )
+
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+*/
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+export const apolloClient = new ApolloClient({
+  ssrMode: true,
+  link: httpLink, // errorLink.concat(httpLink),
+  cache,
+})
