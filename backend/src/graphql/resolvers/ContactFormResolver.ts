d1c8d56b1e14181c6638dcb60aff81de26466277
elweyn <heine.hannes@gmail.com>
2023-12-13 21:56:23 +0100

    Call sendEmail with model structure.

--- backend/src/graphql/resolvers/ContactFormResolver.ts
+++ backend/src/graphql/resolvers/ContactFormResolver.ts
@@ -4,6 +4,7 @@ import { Resolver, Mutation, Query, Arg } from 'type-graphql'
 
 import { createBrevoInstance, createSmtpEmail } from '#api/NewsletterBrevo'
 import { ContactFormInput } from '#inputs/ContactFormInput'
+import { NewsletterUser } from '#src/model/NewsletterUser'
 import { prisma } from '#src/prisma'
 
 @Resolver()
@@ -15,7 +16,28 @@ export class ContactFormResolver {
     await prisma.contactForm.create({ data: contactFormData })
     // code to send email goes here
     const apiInstance: SibApiV3Sdk.TransactionalEmailsApi = createBrevoInstance()
-    const sendSmtpEmail: SibApiV3Sdk.SendSmtpEmail = createSmtpEmail()
+
+    const cc = [
+      new NewsletterUser('Mathias Lenz', 'mathias.lenz@it4c.dev'),
+      new NewsletterUser('Hannes Heine', 'hannes.heine@it4c.dev'),
+      new NewsletterUser('Ulf Gebhardt', 'ulf.gebhardt@it4c.dev'),
+    ]
+    const sendSmtpEmail: SibApiV3Sdk.SendSmtpEmail = createSmtpEmail({
+      subject: 'My {{params.subject}}',
+      emailTo: [
+        new NewsletterUser(
+          contactFormData.firstName + ' ' + contactFormData.lastName,
+          contactFormData.email,
+        ),
+      ],
+      htmlContent:
+        '<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>',
+      sender: new NewsletterUser('DreamMall Earth Team', 'no-reply@dreammall.earth'),
+      cc,
+      replyTo: new NewsletterUser('DreamMall Earth', 'contact@dreammall.earth'),
+      params: { parameter: 'My param value', subject: 'New Subject' },
+      headers: { 'Some-Custom-Name': 'unique-id-1234' },
+    })
 
     void apiInstance.sendTransacEmail(sendSmtpEmail).then(
       function (data) {
