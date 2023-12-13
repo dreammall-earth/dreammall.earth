33393df0533c622a872de6e69593ca80ac06ff4c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-13 12:08:26 +0100

    misstyped story

--- presenter/src/components/sections/ThirdSection.stories.ts
+++ presenter/src/components/sections/ThirdSection.stories.ts
@@ -1,5 +1,3 @@
-import { ConcreteComponent } from 'vue'
-
 import ThirdSection from './ThirdSection.vue'
 
 import type { Meta, StoryObj } from '@storybook/vue3'
@@ -7,7 +5,8 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
   title: 'Sections/Section3',
-  component: ThirdSection as Omit<ConcreteComponent<typeof ThirdSection>, 'props'>,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  component: ThirdSection,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {},
