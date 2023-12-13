1251d76bdc3278dd6ea67d7201b550460c8a6590
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-13 21:11:31 +0100

    build.backend.sh script

---
+++ deployment/build.backend.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Find current directory & configure paths
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+PROJECT_ROOT=$SCRIPT_DIR/../backend
+BUILD_DIR=$PROJECT_ROOT/build
+
+# Build the project
+cd $PROJECT_ROOT
+rm -R $BUILD_DIR
+npm install
+npm run build
