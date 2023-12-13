19fad61ca7a24164ffc19d74a1648ecc7b1718d3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-13 21:17:09 +0100

    migrate.database.sh script

---
+++ deployment/migrate.database.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Find current directory & configure paths
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+PROJECT_ROOT=$SCRIPT_DIR/../backend
+
+# Build the project
+cd $PROJECT_ROOT
+npm install
+npm run db:migrate
