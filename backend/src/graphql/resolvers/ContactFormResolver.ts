d2b75015418b4afdd79477ab53154cae16940ccd
elweyn <heine.hannes@gmail.com>
2023-12-13 11:05:08 +0100

    write first boilerplate code for brevo send TransactionalEmails.

--- backend/src/graphql/resolvers/ContactFormResolver.ts
+++ backend/src/graphql/resolvers/ContactFormResolver.ts
@@ -1,5 +1,5 @@
 import { Resolver, Mutation, Query, Arg } from 'type-graphql'
-
+import * as SibApiV3Sdk from '@getbrevo/brevo'
 import { ContactFormInput } from '#inputs/ContactFormInput'
 import { prisma } from '#src/prisma'
 
@@ -11,6 +11,22 @@ export class ContactFormResolver {
   ): Promise<boolean> {
     await prisma.contactForm.create({ data: contactFormData })
     // code to send email goes here
+    const apiInstance: SibApiV3Sdk.TransactionalEmailsApi = new SibApiV3Sdk.TransactionalEmailsApi()
+    const brevoKey = '12345' // TODO: process.env.BREVO_KEY as string
+    console.log('Brevo API_KEY', brevoKey)
+    apiInstance.setApiKey(SibApiV3Sdk.AccountApiApiKeys.apiKey, brevoKey)
+
+    const sendSmtpEmail: SibApiV3Sdk.SendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail()
+    sendSmtpEmail.subject = ''
+    sendSmtpEmail.htmlContent = ''
+    sendSmtpEmail.sender = {"name":"John Doe","email":"example@example.com"}
+    sendSmtpEmail.to = [{"email":"example@example.com","name":"Jane Doe"}]
+    sendSmtpEmail.cc = [{"email":"example2@example2.com","name":"Janice Doe"}]
+    sendSmtpEmail.bcc = [{"name":"John Doe","email":"example@example.com"}]
+    sendSmtpEmail.replyTo = {"email":"replyto@domain.com","name":"John Doe"}
+    sendSmtpEmail.headers = {"Some-Custom-Name":"unique-id-1234"}
+    sendSmtpEmail.params = {"parameter":"My param value","subject":"New Subject"}
+
     return true
   }
 
