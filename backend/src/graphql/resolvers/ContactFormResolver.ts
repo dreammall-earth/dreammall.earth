00e292de4dd71e18b05ddd3bbb31f3ecbb160bd9
elweyn <heine.hannes@gmail.com>
2023-12-13 12:49:38 +0100

    Switch from brewo modul to sendinblue typescript part.

--- backend/src/graphql/resolvers/ContactFormResolver.ts
+++ backend/src/graphql/resolvers/ContactFormResolver.ts
@@ -1,5 +1,7 @@
+// eslint-disable-next-line import/no-namespace
+import * as SibApiV3Sdk from '@sendinblue/client'
 import { Resolver, Mutation, Query, Arg } from 'type-graphql'
-import * as SibApiV3Sdk from '@getbrevo/brevo'
+
 import { ContactFormInput } from '#inputs/ContactFormInput'
 import { prisma } from '#src/prisma'
 
@@ -14,35 +16,39 @@ export class ContactFormResolver {
     const apiInstance = this.createBrevoInstance()
     const sendSmtpEmail = this.createSmtpEmail()
 
-    apiInstance.sendTransacEmail(sendSmtpEmail).then((data) => {
-      console.log('API called successfully. Returned data: ' + data)
-      return true
-    }, function(error) {
+    try {
+      void apiInstance.sendTransacEmail(sendSmtpEmail).then(function (data) {
+        // eslint-disable-next-line no-console
+        console.log('API called successfully. Returned data: ', JSON.stringify(data))
+        return true
+      })
+    } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error)
-    })
-    return false
+      return false
+    }
+    return true
   }
 
   private createBrevoInstance(): SibApiV3Sdk.TransactionalEmailsApi {
-    const SibApiV3Sdk = require('@getbrevo/brevo')
     const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi()
-    const apiKey = apiInstance.authentications['apiKey']
-    apiKey.apiKey = '12345'
+    apiInstance.setApiKey(SibApiV3Sdk.AccountApiApiKeys.apiKey, '12345')
     return apiInstance
   }
 
   private createSmtpEmail(): SibApiV3Sdk.SendSmtpEmail {
     const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail()
 
-    sendSmtpEmail.subject = "My {{params.subject}}"
-    sendSmtpEmail.htmlContent = "<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>";
-    sendSmtpEmail.sender = { "name": "John Doe", "email": "example@example.com" }
-    sendSmtpEmail.to = [{ "email": "hannes.heine@it4c.dev", "name": "Hannes Heine" }]
-    sendSmtpEmail.cc = [{ "email": "mathias.lenz@it4c.dev", "name": "Mathias Lenz" }]
-    sendSmtpEmail.bcc = [{ "name": "Ulf Gebhardt", "email": "ulf.gebhardt@it4c.dev" }]
-    sendSmtpEmail.replyTo = { "email": "info@it4c.dev", "name": "IT4C Support" }
-    sendSmtpEmail.headers = { "Some-Custom-Name": "unique-id-1234" }
-    sendSmtpEmail.params = { "parameter": "My param value", "subject": "New Subject" }
+    sendSmtpEmail.subject = 'My {{params.subject}}'
+    sendSmtpEmail.htmlContent =
+      '<html><body><h1>This is my first transactional email {{params.parameter}}</h1></body></html>'
+    sendSmtpEmail.sender = { name: 'John Doe', email: 'example@example.com' }
+    sendSmtpEmail.to = [{ email: 'hannes.heine@it4c.dev', name: 'Hannes Heine' }]
+    sendSmtpEmail.cc = [{ email: 'mathias.lenz@it4c.dev', name: 'Mathias Lenz' }]
+    sendSmtpEmail.bcc = [{ name: 'Ulf Gebhardt', email: 'ulf.gebhardt@it4c.dev' }]
+    sendSmtpEmail.replyTo = { email: 'info@it4c.dev', name: 'IT4C Support' }
+    sendSmtpEmail.headers = { 'Some-Custom-Name': 'unique-id-1234' }
+    sendSmtpEmail.params = { parameter: 'My param value', subject: 'New Subject' }
 
     return sendSmtpEmail
   }
