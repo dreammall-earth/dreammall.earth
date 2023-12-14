ef20b9ae3e50f35542dd828ee89bcebe321b56aa
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-14 16:14:31 +0100

    prettier fixes

--- presenter/scripts/tests/mock.IntersectionObserver.ts
+++ presenter/scripts/tests/mock.IntersectionObserver.ts
@@ -8,30 +8,24 @@ export function setupIntersectionObserverMock({
   unobserve = () => null,
 } = {}): void {
   class MockIntersectionObserver implements IntersectionObserver {
-    readonly root: Element | null = root;
-    readonly rootMargin: string = rootMargin;
-    readonly thresholds: ReadonlyArray < number > = thresholds;
-    disconnect: () => void = disconnect;
-    observe: (target: Element) => void = observe;
-    takeRecords: () => IntersectionObserverEntry[] = takeRecords;
-    unobserve: (target: Element) => void = unobserve;
+    readonly root: Element | null = root
+    readonly rootMargin: string = rootMargin
+    readonly thresholds: ReadonlyArray<number> = thresholds
+    disconnect: () => void = disconnect
+    observe: (target: Element) => void = observe
+    takeRecords: () => IntersectionObserverEntry[] = takeRecords
+    unobserve: (target: Element) => void = unobserve
   }
 
-  Object.defineProperty(
-    window,
-    'IntersectionObserver', {
-      writable: true,
-      configurable: true,
-      value: MockIntersectionObserver
-    }
-  );
+  Object.defineProperty(window, 'IntersectionObserver', {
+    writable: true,
+    configurable: true,
+    value: MockIntersectionObserver,
+  })
 
-  Object.defineProperty(
-    global,
-    'IntersectionObserver', {
-      writable: true,
-      configurable: true,
-      value: MockIntersectionObserver
-    }
-  );
+  Object.defineProperty(global, 'IntersectionObserver', {
+    writable: true,
+    configurable: true,
+    value: MockIntersectionObserver,
+  })
 }
