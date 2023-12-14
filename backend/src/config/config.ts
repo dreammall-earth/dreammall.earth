a4755da27b29a40b9c611e0eced30468fb872c50
elweyn <heine.hannes@gmail.com>
2023-12-14 21:24:07 +0100

    Add BREVO_TEMPLATES to env.dist

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -11,12 +11,16 @@ interface ENV {
   BREVO_KEY: string | undefined
   BREVO_CONTACT_REQUEST_TO_NAME: string | undefined
   BREVO_CONTACT_REQUEST_TO_EMAIL: string | undefined
+  BREVO_TEMPLATE_CONTACT_BASE: number | undefined
+  BREVO_TEMPLATE_CONTACT_USER: number | undefined
 }
 
 interface Config {
   BREVO_KEY: string
   BREVO_CONTACT_REQUEST_TO_NAME: string
   BREVO_CONTACT_REQUEST_TO_EMAIL: string
+  BREVO_TEMPLATE_CONTACT_BASE: number
+  BREVO_TEMPLATE_CONTACT_USER: number
 }
 
 /* eslint-disable n/no-process-env */
@@ -25,6 +29,12 @@ const getConfig = (): ENV => {
     BREVO_KEY: process.env.BREVO_KEY,
     BREVO_CONTACT_REQUEST_TO_NAME: process.env.BREVO_CONTACT_REQUEST_TO_NAME,
     BREVO_CONTACT_REQUEST_TO_EMAIL: process.env.BREVO_CONTACT_REQUEST_TO_EMAIL,
+    BREVO_TEMPLATE_CONTACT_BASE: process.env.BREVO_TEMPLATE_CONTACT_BASE
+      ? Number(process.env.BREVO_TEMPLATE_CONTACT_BASE)
+      : undefined,
+    BREVO_TEMPLATE_CONTACT_USER: process.env.BREVO_TEMPLATE_CONTACT_USER
+      ? Number(process.env.BREVO_TEMPLATE_CONTACT_USER)
+      : undefined,
   }
 }
 /* eslint-enable n/no-process-env */
