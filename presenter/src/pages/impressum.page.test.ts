27a18312c9f5f016d4895e01dc4614a92330027b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-14 11:34:55 +0100

    fixes according to recent merges

--- presenter/src/pages/impressum.page.test.ts
+++ presenter/src/pages/impressum.page.test.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
-import { h } from 'vue'
+import { Component, h } from 'vue'
 import { VApp } from 'vuetify/components'
 
 import ImpressSection from '#components/sections/ImpressSection.vue'
@@ -10,8 +10,7 @@ import ImpressumPage from './impressum.page.vue'
 describe('ImpressumPage', () => {
   const wrapper = mount(VApp, {
     slots: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      default: h(ImpressumPage),
+      default: h(ImpressumPage as Component),
     },
   })
 
