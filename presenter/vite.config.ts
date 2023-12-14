cd7fec3d760fba1a2ae478653f294d122622be91
Moriz Wahl <moriz.wahl@gmx.de>
2023-12-14 22:06:37 +0100

    add #mutations path

--- presenter/vite.config.ts
+++ presenter/vite.config.ts
@@ -37,6 +37,7 @@ const config: UserConfig = {
       '#plugins': path.join(__dirname, '/renderer/plugins'),
       '#context': path.join(__dirname, '/renderer/context'),
       '#types': path.join(__dirname, '/types'),
+      '#mutations': path.join(__dirname, '/src/graphql/mutations'),
       '#root': __dirname,
     },
   },
