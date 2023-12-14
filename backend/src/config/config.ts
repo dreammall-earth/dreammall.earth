002dcfb4728203c99670b3882c88515f64d18296
elweyn <heine.hannes@gmail.com>
2023-12-14 13:09:12 +0100

    Add Brevo to name and email.

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -9,16 +9,22 @@ config({
 
 interface ENV {
   BREVO_KEY: string | undefined
+  BREVO_CONTACT_REQUEST_TO_NAME: string | undefined
+  BREVO_CONTACT_REQUEST_TO_EMAIL: string | undefined
 }
 
 interface Config {
   BREVO_KEY: string
+  BREVO_CONTACT_REQUEST_TO_NAME: string
+  BREVO_CONTACT_REQUEST_TO_EMAIL: string
 }
 
 /* eslint-disable n/no-process-env */
 const getConfig = (): ENV => {
   return {
     BREVO_KEY: process.env.BREVO_KEY,
+    BREVO_CONTACT_REQUEST_TO_NAME: process.env.BREVO_CONTACT_REQUEST_TO_NAME,
+    BREVO_CONTACT_REQUEST_TO_EMAIL: process.env.BREVO_CONTACT_REQUEST_TO_EMAIL,
   }
 }
 /* eslint-enable n/no-process-env */
