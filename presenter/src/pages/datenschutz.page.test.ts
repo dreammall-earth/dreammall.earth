eae619a21a268ec8969debea90eb87e335055c4d
Markus <57147781+maeckes-infinitylabs@users.noreply.github.com>
2023-12-14 11:55:54 +0100

    Update presenter/src/pages/datenschutz.page.test.ts
    Co-authored-by: Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>

--- presenter/src/pages/datenschutz.page.test.ts
+++ presenter/src/pages/datenschutz.page.test.ts
@@ -10,8 +10,7 @@ import DataPrivacyPage from './datenschutz.page.vue'
 describe('DataPrivacyPage', () => {
   const wrapper = mount(VApp, {
     slots: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      default: h(DataPrivacyPage),
+      default: h(DataPrivacyPage as Component),
     },
   })
 
