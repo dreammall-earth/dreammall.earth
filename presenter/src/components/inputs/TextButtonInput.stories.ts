f35a708358b18e373d80df189bb6e07c0b3d430c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-14 01:00:41 +0100

    SBComp and fixes

--- presenter/src/components/inputs/TextButtonInput.stories.ts
+++ presenter/src/components/inputs/TextButtonInput.stories.ts
@@ -1,3 +1,5 @@
+import { SBComp } from '#types/SBComp'
+
 import TextButtonInput from './TextButtonInput.vue'
 
 import type { Meta, StoryObj } from '@storybook/vue3'
@@ -5,8 +7,7 @@ import type { Meta, StoryObj } from '@storybook/vue3'
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 const meta = {
   title: 'Inputs/TextButtonInput',
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  component: TextButtonInput,
+  component: TextButtonInput as SBComp,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
   tags: ['autodocs'],
   argTypes: {
