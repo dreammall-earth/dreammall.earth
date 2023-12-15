aa840d8d74caaf7098011c4f3d5b8ecc44ae4d21
elweyn <heine.hannes@gmail.com>
2023-12-15 10:55:05 +0100

    Clear all mocks to setup the NewsletterBrevo to call the mocked API.

--- backend/src/api/NewsletterBrevo.spec.ts
+++ backend/src/api/NewsletterBrevo.spec.ts
@@ -9,7 +9,6 @@ import { sendContactFormEmail } from './NewsletterBrevo'
 
 jest.mock('#config/config', () => {
   return {
-    // TODO: To tests the NewsletterBrevo we need a value and mock setApiKey correctly
     BREVO_KEY: '',
     BREVO_CONTACT_REQUEST_TO_NAME: 'Peter Lustig',
     BREVO_CONTACT_REQUEST_TO_EMAIL: 'peter@lustig.de',
@@ -63,8 +62,7 @@ describe('NewsletterBrevo', () => {
 
   describe('call successful', () => {
     beforeEach(() => {
-      jest.resetAllMocks()
-      // TODO: This does lead to the Brevo API been called
+      jest.clearAllMocks()
       config.BREVO_KEY = '1234'
       sendContactFormEmail(contactForm)
     })
