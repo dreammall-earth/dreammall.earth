0103dd7f98d4f011fec4e10addf23c6d9bcedee6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-17 02:07:39 +0100

    deliver files compressed at build time, cache policy, in development compress on the fly

--- presenter/server/index.ts
+++ presenter/server/index.ts
@@ -24,14 +24,18 @@ void startServer()
 async function startServer() {
   const app = express()
 
-  app.use(compression())
-
   // Vite integration
   if (isProduction) {
     // In production, we need to serve our static assets ourselves.
     // (In dev, Vite's middleware serves our static assets.)
     const sirv = (await import('sirv')).default
-    app.use(sirv(`${root}/build/client`))
+    app.use(
+      sirv(`${root}/build/client`, {
+        maxAge: 31536000, // 1Y
+        immutable: true,
+        gzip: true,
+      }),
+    )
   } else {
     // We instantiate Vite's development server and integrate its middleware to our server.
     // ⚠️ We instantiate it only in development. (It isn't needed in production and it
@@ -44,6 +48,9 @@ async function startServer() {
       })
     ).middlewares
     app.use(viteDevMiddleware)
+
+    // on the fly compression
+    app.use(compression())
   }
 
   // ...
