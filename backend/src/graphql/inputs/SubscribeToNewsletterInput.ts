d23b19374a497a9011357f1121f432b9941434c7
Moriz Wahl <moriz.wahl@gmx.de>
2023-12-19 13:19:30 +0100

    add validation rules for inputs of contact form and newsletter subscription

--- backend/src/graphql/inputs/SubscribeToNewsletterInput.ts
+++ backend/src/graphql/inputs/SubscribeToNewsletterInput.ts
@@ -1,15 +1,23 @@
-import { IsEmail } from 'class-validator'
+import { IsEmail, MaxLength } from 'class-validator'
 import { InputType, Field } from 'type-graphql'
+
+// values according to prisma/schema.prisma
+const MAX_CHARS_FIRST_NAME = 50
+const MAX_CHARS_LAST_NAME = 50
+const MAX_CHARS_EMAIL = 255
 
 @InputType({ description: 'Input for the newsletter subscription form' })
 export class SubscribeToNewsletterInput {
   @Field()
+  @MaxLength(MAX_CHARS_FIRST_NAME)
   firstName: string
 
   @Field()
+  @MaxLength(MAX_CHARS_LAST_NAME)
   lastName: string
 
   @Field()
   @IsEmail()
+  @MaxLength(MAX_CHARS_EMAIL)
   email: string
 }
