5c2ca5771da19efe79a4717fa85ab7a10c95ee84
Markus <markus@infinity.labs.ooo>
2023-12-20 17:05:18 +0100

    improved third section

--- presenter/src/components/layout/SectionColumnCard.vue
+++ presenter/src/components/layout/SectionColumnCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="mb-2" flat color="transparent">
+  <v-card class="mb-2 w-100" width="375" flat color="transparent">
     <v-img height="100" :src="props.imageSrc" class="text-white"></v-img>
     <v-card-title class="card-title mt-8">{{ props.cardTitle }}</v-card-title>
     <v-card-text class="card-text mt-8">{{ props.cardText }}</v-card-text>
@@ -34,7 +34,7 @@ const props = withDefaults(
 $mobile: 500px;
 
 .v-card {
-  max-width: 320px;
+  max-width: 375px;
   font-family: Poppins, sans-serif;
   font-size: 1.25rem;
   font-style: normal;
@@ -48,11 +48,8 @@ $mobile: 500px;
     font-weight: 600;
     line-height: 125%; /* 2.03125rem */
     text-transform: capitalize;
-
-    @media screen and (max-width: $mobile) {
-      text-overflow: unset;
-      text-wrap: balance;
-    }
+    text-overflow: unset;
+    text-wrap: balance;
   }
 }
 </style>
