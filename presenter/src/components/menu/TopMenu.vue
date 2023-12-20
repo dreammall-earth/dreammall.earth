0ec7b02b795c6e5f3cd664bb01bd0f011a8c3304
Markus <markus@infinity.labs.ooo>
2023-12-20 15:01:57 +0100

    [feature] added mobile burger menu

--- presenter/src/components/menu/TopMenu.vue
+++ presenter/src/components/menu/TopMenu.vue
@@ -1,23 +1,67 @@
 <template>
-  <v-app-bar :color="appBackground" flat class="topmenu">
-    <v-row>
-      <v-col>
-        <a href="/">
-          <LogoImage class="ml-8" />
-        </a>
-      </v-col>
-      <v-col class="d-none d-md-flex align-center justify-center grow">
-        <AnchorLink class="mx-4" href="/#about" :label="$t('menu.header.about')"></AnchorLink>
-        <AnchorLink class="mx-4" href="/#products" :label="$t('menu.header.products')"></AnchorLink>
+  <div class="topmenu">
+    <v-app-bar :color="appBackground" flat>
+      <v-row>
+        <v-col class="d-flex align-center">
+          <a href="/" class="w-100 ml-8">
+            <LogoImage class="" />
+          </a>
+        </v-col>
+        <v-col class="d-none d-md-flex align-center justify-center grow">
+          <!-- TODO same as below: refactor -->
+          <AnchorLink class="mx-4" href="/#about" :label="$t('menu.header.about')"></AnchorLink>
+          <AnchorLink
+            class="mx-4"
+            href="/#products"
+            :label="$t('menu.header.products')"
+          ></AnchorLink>
+          <AnchorLink
+            class="mx-4"
+            href="/#contactname"
+            :label="$t('menu.header.contact')"
+          ></AnchorLink>
+        </v-col>
+        <v-col class="d-flex">
+          <div class="d-flex d-md-none align-center justify-end w-100 mr-8">
+            <v-img
+              class="mobile-menu-icon w-100"
+              :src="MobileMenuIcon"
+              @click="toggleNavBar"
+            />
+          </div>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="mobileMenu"
+      :color="navBackground"
+      location="right"
+      class="nav-drawer d-flex d-md-none"
+    >
+      <div class="d-flex flex-column py-8">
+        <!-- TODO same as above: refactor -->
         <AnchorLink
-          class="mx-4"
+          class="ma-4"
+          href="/#about"
+          :label="$t('menu.header.about')"
+          @click="mobileMenu = !mobileMenu"
+        ></AnchorLink>
+        <AnchorLink
+          class="ma-4"
+          href="/#products"
+          :label="$t('menu.header.products')"
+          @click="mobileMenu = !mobileMenu"
+        ></AnchorLink>
+        <AnchorLink
+          class="ma-4"
           href="/#contactname"
           :label="$t('menu.header.contact')"
+          @click="mobileMenu = !mobileMenu"
         ></AnchorLink>
-      </v-col>
-      <v-col class="d-none d-md-flex"> </v-col>
-    </v-row>
-  </v-app-bar>
+      </div>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -26,12 +70,26 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import AnchorLink from '#components/nav/AnchorLink.vue'
 
 import LogoImage from './LogoImage.vue'
+import MobileMenuIcon from '#assets/img/hamburger_mobile.svg'
 
 const appBackground = ref('transparent')
+const navBackground = ref('#d8d8d8')
+const mobileMenu = ref(false)
 
 function changeAppBarBackground() {
   if (appBackground.value !== '#f5f5f5') {
     // TODO change class for smoother transition
+    appBackground.value = '#f5f5f5'
+    navBackground.value = '#f5f5f5'
+  }
+}
+
+function toggleNavBar() {
+  mobileMenu.value = !mobileMenu.value
+
+  if (navBackground.value !== '#f5f5f5') {
+    // TODO change class for smoother transition
+    navBackground.value = '#f5f5f5'
     appBackground.value = '#f5f5f5'
   }
 }
@@ -54,6 +112,17 @@ onUnmounted(() => {
 .topmenu {
   .v-toolbar__content {
     height: 95px !important;
+  }
+  .mobile-menu-icon {
+    max-width: 35px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .nav-drawer {
+    top: 95px !important;
+    height: fit-content !important;
   }
 }
 </style>
