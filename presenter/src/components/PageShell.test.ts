8a8606a7e1a0e3a8546e5034fdc0ce1dcbed9a00
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-01-16 10:28:54 +0100

    lint

--- presenter/src/components/PageShell.test.ts
+++ presenter/src/components/PageShell.test.ts
@@ -1,7 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 
-import TopMenu from '#components/menu/TopMenu.vue'
 import { setupIntersectionObserverMock } from '#root/scripts/tests/mock.IntersectionObserver'
 
 import PageShell from './PageShell.vue'
