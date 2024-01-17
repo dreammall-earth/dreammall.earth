024e68ba570794986fa2685c243a071484030d56
mahula <lenzmath@posteo.de>
2024-01-17 21:48:39 +0100

    add snapshots and unit tests for data privacy checkbox component

---
+++ presenter/src/components/controls/DataPrivacyCheckbox.test.ts
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import DataPrivacyCheckbox from './DataPrivacyCheckbox.vue'
+
+describe('DataPrivacyCheckbox', () => {
+  const Wrapper = () => {
+    return mount(DataPrivacyCheckbox)
+  }
+  let wrapper: ReturnType<typeof Wrapper>
+
+  beforeEach(() => {
+    wrapper = Wrapper()
+  })
+
+  it('renders', () => {
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('emits the modelValue:update event on checkbox change', async () => {
+    await wrapper.find('input[type="checkbox"]').trigger('change')
+    expect(wrapper.emitted('modelValue:update')).toMatchObject([[1]])
+  })
+})
