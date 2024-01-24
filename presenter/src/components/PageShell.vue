a6835ce2b1d63f648758b0a273a9334d4dd64200
Markus <markus@infinity.labs.ooo>
2024-01-24 16:19:01 +0100

    moved headermenu to layout file

--- presenter/src/components/PageShell.vue
+++ presenter/src/components/PageShell.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app>
-    <HeaderMenu />
     <slot />
   </v-app>
 </template>
 
-<script lang="ts" setup>
-import HeaderMenu from '#components/menu/HeaderMenu.vue'
-</script>
+<script lang="ts" setup></script>
