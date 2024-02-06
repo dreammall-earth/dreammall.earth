8c9a3790b2b5c801fc0701c6914b0a116bbd2dfd
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-06 17:12:51 +0100

    revert changes

--- deployment/start.backend.sh
+++ deployment/start.backend.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(dirname $SCRIPT_PATH)/../backend
 LOG_FILE=$SCRIPT_DIR/../log/$(date +"%Y-%m-%d_%T")_pm2.backend.log
 
 cd $SCRIPT_DIR
-NODE_ENV=production pm2 start --name backend "Z=UTC TS_NODE_BASEURL=./build node -r tsconfig-paths/register build/src/index.js" -l $LOG_FILE --log-date-format 'YYYY-MM-DD HH:mm:ss.SSS'
+NODE_ENV=production pm2 start --name backend "npm run start" -l $LOG_FILE --log-date-format 'YYYY-MM-DD HH:mm:ss.SSS'
 pm2 save
