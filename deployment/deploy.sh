1f95abe82a7f7a384881f21b7fe4995b2da0ce52
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-06 16:25:31 +0100

    using global redirect to console statement

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,34 +8,36 @@ LOG_ERROR_FILE=$SCRIPT_DIR/../log/$(date +"%Y-%m-%d_%T")_deploy.error.log
 
 exec 3>&1 1>>${LOG_FILE} 2>&1 2>>${LOG_ERROR_FILE}
 
-echo 'Start Deploy' | tee /dev/fd/3
+{
+    echo 'Start Deploy' 
 
-# Update git
-# assuming you are already on the right branch
-git pull -ff | tee /dev/fd/3
+    # Update git
+    # assuming you are already on the right branch
+    git pull -ff
 
-# stop all services
-pm2 stop all | tee /dev/fd/3
-pm2 delete all | tee /dev/fd/3
-pm2 save | tee /dev/fd/3
+    # stop all services
+    pm2 stop all
+    pm2 delete all
+    pm2 save
 
-# run as production
-BACKUP_NODE_ENV=$NODE_ENV
-export NODE_ENV=production
+    # run as production
+    BACKUP_NODE_ENV=$NODE_ENV
+    export NODE_ENV=production
 
-# Backend & Database Migration
-$SCRIPT_DIR/build.backend.sh | tee /dev/fd/3
-$SCRIPT_DIR/migrate.database.sh | tee /dev/fd/3
-$SCRIPT_DIR/start.backend.sh | tee /dev/fd/3
+    # Backend & Database Migration
+    $SCRIPT_DIR/build.backend.sh
+    $SCRIPT_DIR/migrate.database.sh
+    $SCRIPT_DIR/start.backend.sh
 
-# Presenter
-$SCRIPT_DIR/build.presenter.sh | tee /dev/fd/3
-$SCRIPT_DIR/start.presenter.sh | tee /dev/fd/3
+    # Presenter
+    $SCRIPT_DIR/build.presenter.sh
+    $SCRIPT_DIR/start.presenter.sh
 
-# Docs
-$SCRIPT_DIR/build.docs.sh | tee /dev/fd/3
+    # Docs
+    $SCRIPT_DIR/build.docs.sh
 
-# restore node env
-NODE_ENV=$BACKUP_NODE_ENV
+    # restore node env
+    NODE_ENV=$BACKUP_NODE_ENV
 
-echo 'Finish Deploy' | tee /dev/fd/3
+    echo 'Finish Deploy'
+} | tee /dev/fd/3
