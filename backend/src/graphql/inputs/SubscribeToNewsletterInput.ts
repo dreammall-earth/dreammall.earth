fb8ca19bf5376a35bbdb8a166a08057863886416
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-19 13:49:08 +0100

    refactor constant defintion to be in a unified file

--- backend/src/graphql/inputs/SubscribeToNewsletterInput.ts
+++ backend/src/graphql/inputs/SubscribeToNewsletterInput.ts
@@ -1,23 +1,20 @@
 import { IsEmail, MaxLength } from 'class-validator'
 import { InputType, Field } from 'type-graphql'
 
-// values according to prisma/schema.prisma
-const MAX_CHARS_FIRST_NAME = 50
-const MAX_CHARS_LAST_NAME = 50
-const MAX_CHARS_EMAIL = 254
+import { VALIDATION } from '#config/constants'
 
 @InputType({ description: 'Input for the newsletter subscription form' })
 export class SubscribeToNewsletterInput {
   @Field()
-  @MaxLength(MAX_CHARS_FIRST_NAME)
+  @MaxLength(VALIDATION.MAX_CHARS_FIRST_NAME)
   firstName: string
 
   @Field()
-  @MaxLength(MAX_CHARS_LAST_NAME)
+  @MaxLength(VALIDATION.MAX_CHARS_LAST_NAME)
   lastName: string
 
   @Field()
   @IsEmail()
-  @MaxLength(MAX_CHARS_EMAIL)
+  @MaxLength(VALIDATION.MAX_CHARS_EMAIL)
   email: string
 }
