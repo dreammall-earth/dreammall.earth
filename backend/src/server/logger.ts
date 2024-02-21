14b4755b07d7291531844551a27c1554de3906d8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-21 12:31:47 +0100

    apollo logging

---
+++ backend/src/server/logger.ts
@@ -0,0 +1,35 @@
+/* class BasicLogging {
+  requestDidStart({queryString, parsedQuery, variables}) {
+    const query = queryString || print(parsedQuery);
+    console.log(query);
+    console.log(variables);
+  }
+
+  willSendResponse({graphqlResponse}) {
+    console.log(JSON.stringify(graphqlResponse, null, 2));
+  }
+} */
+
+import {
+  GraphQLRequest,
+  GraphQLRequestContext,
+  GraphQLRequestContextWillSendResponse,
+} from '@apollo/server'
+
+export default {
+  // Fires whenever a GraphQL request is received from a client.
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async requestDidStart(requestContext: GraphQLRequestContext<GraphQLRequest>) {
+    // eslint-disable-next-line no-console
+    console.log('Apollo Request', requestContext.request)
+    return {
+      // eslint-disable-next-line @typescript-eslint/require-await
+      async willSendResponse(
+        requestContext: GraphQLRequestContextWillSendResponse<GraphQLRequest>,
+      ) {
+        // eslint-disable-next-line no-console
+        console.log('Apollo Response', requestContext.response)
+      },
+    }
+  },
+}
