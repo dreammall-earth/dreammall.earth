2c0b77de3e7174f7ff17d01f68c46c2bd2ba65f9
Moriz Wahl <moriz.wahl@gmx.de>
2024-02-21 10:22:06 +0100

    add LDAP to config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -29,7 +29,14 @@ const BREVO = {
     : undefined,
 }
 
-export const CONFIG = { ...BREVO }
+const LDAP = {
+  LDAP_URL: process.env.LDAP_URL || 'ldap://localhost:389',
+  LDAP_ADMIN_DN: process.env.LDAP_ADMIN_DN || '',
+  LDAP_ADMIN_PASSWORD: process.env.LDAP_ADMIN_PASSWORD || '',
+}
+
+
+export const CONFIG = { ...BREVO, ...LDAP }
 
 // Config Checks
 export const CONFIG_CHECKS = {
