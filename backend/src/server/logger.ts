0adb5377d0f09ac8f72e7386c89caec9912fba0a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-22 17:42:00 +0100

    lint fixes

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -1,22 +1,21 @@
-import logger from '#src/logger'
 import {
   GraphQLRequest,
   GraphQLRequestContext,
   GraphQLRequestContextWillSendResponse,
 } from '@apollo/server'
 
+import logger from '#src/logger'
+
 export default {
   // Fires whenever a GraphQL request is received from a client.
   // eslint-disable-next-line @typescript-eslint/require-await
   async requestDidStart(requestContext: GraphQLRequestContext<GraphQLRequest>) {
-    // eslint-disable-next-line no-console
     logger.debug('Apollo Request', requestContext.request)
     return {
       // eslint-disable-next-line @typescript-eslint/require-await
       async willSendResponse(
         requestContext: GraphQLRequestContextWillSendResponse<GraphQLRequest>,
       ) {
-        // eslint-disable-next-line no-console
         logger.debug('Apollo Response', requestContext.request, requestContext.response.body)
       },
     }
