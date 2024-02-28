5591464c7561bb66a6754a6d0219a74101699e9b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-28 14:56:03 +0100

    authStore storing user object in pinia store

---
+++ presenter/src/stores/authStore.ts
@@ -0,0 +1,30 @@
+import { User } from 'oidc-client-ts'
+import { acceptHMRUpdate, defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+export const authStore = defineStore('auth', () => {
+  const authUser = ref<User | null>(null)
+
+  const accessToken = computed(() => authUser.value?.access_token ?? '')
+
+  const isLoggedIn = computed(() => !!authUser.value)
+
+  const save = (user: User) => {
+    authUser.value = user
+  }
+
+  const clear = () => {
+    authUser.value = null
+  }
+
+  return {
+    accessToken,
+    isLoggedIn,
+    save,
+    clear,
+  }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(authStore, import.meta.hot))
+}
