802fe9d28f553b95067c7f3581f8dfd83cd43457
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-28 14:56:16 +0100

    env variables

--- presenter/src/env.ts
+++ presenter/src/env.ts
@@ -1,3 +1,18 @@
+const AUTH = {
+  AUTHORITY: (import.meta.env.PUBLIC_ENV__AUTH__AUTHORITY ?? 'http://localhost:9000') as string,
+  AUTHORITY_SIGNUP_URI: (import.meta.env.PUBLIC_ENV__AUTH__AUTHORITY_SIGNUP_URI ??
+    'http://localhost:9000/signup') as string,
+  CLIENT_ID: (import.meta.env.PUBLIC_ENV__AUTH__CLIENT_ID ?? 'dreammall-presenter') as string,
+  REDIRECT_URI: (import.meta.env.PUBLIC_ENV__AUTH__REDIRECT_URI ??
+    'http://localhost:3000/auth') as string,
+  SILENT_REDIRECT_URI: (import.meta.env.PUBLIC_ENV__AUTH__SILENT_REDIRECT_URI ??
+    'http://localhost:3000/silent-refresh') as string,
+  POST_LOGOUT_REDIRECT_URI: (import.meta.env.PUBLIC_ENV__AUTH__POST_LOGOUT_REDIRECT_URI ??
+    'http://localhost:3000') as string,
+  RESPONSE_TYPE: (import.meta.env.PUBLIC_ENV__AUTH__RESPONSE_TYPE ?? 'code') as string,
+  SCOPE: (import.meta.env.PUBLIC_ENV__AUTH__SCOPE ?? 'openid profile posts') as string,
+}
+
 const ENDPOINTS = {
   GRAPHQL_URI: (import.meta.env.PUBLIC_ENV__ENDPOINTS__GRAPHQL_URI ??
     'http://localhost:4000/') as string,
@@ -12,4 +27,4 @@ const META = {
   DEFAULT_TITLE: (import.meta.env.PUBLIC_ENV__META__DEFAULT_TITLE ?? 'DreamMall') as string,
 }
 
-export { ENDPOINTS, META }
+export { AUTH, ENDPOINTS, META }
