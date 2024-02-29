63d0016ac35c3d1787ef10700508e087dd19cf1d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-29 18:56:56 +0100

    store persistence

--- presenter/src/stores/authStore.ts
+++ presenter/src/stores/authStore.ts
@@ -2,28 +2,32 @@ import { User } from 'oidc-client-ts'
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', () => {
-  const authUser = ref<User | null>(null)
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const user = ref<User | null>(null)
 
-  const accessToken = computed(() => authUser.value?.access_token ?? '')
+    const accessToken = computed(() => user.value?.access_token ?? '')
+    const isLoggedIn = computed(() => !!user.value)
 
-  const isLoggedIn = computed(() => !!authUser.value)
+    const save = (u: User) => {
+      user.value = u
+    }
 
-  const save = (user: User) => {
-    authUser.value = user
-  }
+    const clear = () => {
+      user.value = null
+    }
 
-  const clear = () => {
-    authUser.value = null
-  }
-
-  return {
-    accessToken,
-    isLoggedIn,
-    save,
-    clear,
-  }
-})
+    return {
+      user,
+      accessToken,
+      isLoggedIn,
+      save,
+      clear,
+    }
+  },
+  { persist: true},
+)
 
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
