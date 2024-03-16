a89eb31cf636caf6b01d2978620300fa785a46c1
elweyn <heine.hannes@gmail.com>
2024-03-16 10:05:14 +0100

    Change export function to export const title.

--- presenter/src/pages/optin/+title.ts
+++ presenter/src/pages/optin/+title.ts
@@ -1,11 +1,11 @@
-import { PageContext } from 'vike/types'
+// const title = pageContext.locale === 'de' ? 'DreamMall | Newsletter' : 'DreamMall | Newsletter'
+export const title = 'DreamMall | Newsletter'
 
-// export const title = 'DreamMall | Newsletter'
-
-export function title(pageContext: PageContext) {
-  //   const title = pageContext.locale === 'de' ? 'DreamMall | Newsletter' : 'DreamMall | Newsletter'
-  // eslint-disable-next-line no-console
-  console.log('pageContext', pageContext)
-  const title = 'DreamMall | Newsletter'
-  return title
-}
+// import { PageContext } from 'vike/types'
+// export function title(pageContext: PageContext) {
+//   //   const title = pageContext.locale === 'de' ? 'DreamMall | Newsletter' : 'DreamMall | Newsletter'
+//   // eslint-disable-next-line no-console
+//   console.log('pageContext', pageContext)
+//   const title = 'DreamMall | Newsletter'
+//   return title
+// }
