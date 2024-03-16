f547fe86903537097b41c5a3cd3b4ef1c59ece14
elweyn <heine.hannes@gmail.com>
2024-03-16 09:48:35 +0100

    Set i18n.global.locale.value to the value extracted out of the url.

--- presenter/renderer/+onBeforeRoute.ts
+++ presenter/renderer/+onBeforeRoute.ts
@@ -1,13 +1,17 @@
 import { PageContext } from 'vike/types'
 
+import i18n from '#plugins/i18n'
 import { extractLocale } from '#root/locales/extractLocale'
 
 export function onBeforeRoute(pageContext: PageContext) {
+  if (pageContext.urlPathname.endsWith('.js'))
+    return { pageContext: { urlLogical: pageContext.urlPathname } }
   const { urlWithoutLocale, locale } = extractLocale(pageContext.urlPathname)
   // eslint-disable-next-line no-console
   console.log('urlWithoutLocale', urlWithoutLocale)
   // eslint-disable-next-line no-console
   console.log('locale', locale)
+  i18n.global.locale.value = locale
   return {
     pageContext: {
       // Make `locale` available as `pageContext.locale`
