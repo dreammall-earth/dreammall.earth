85a6817604274306b1b811a3db56c379f2b3ef95
elweyn <heine.hannes@gmail.com>
2024-03-18 10:25:58 +0100

    Remove changes on +title.ts

--- presenter/src/pages/_error/+title.ts
+++ presenter/src/pages/_error/+title.ts
@@ -1,6 +1,1 @@
-// import { usePageContext } from '#context/usePageContext'
-
-// const pageContext = usePageContext()
-
-// export const title = pageContext.locale === 'de' ? 'DreamMall | Fehler' : ''
 export const title = 'DreamMall | Fehler'
--- presenter/src/pages/optin/+title.ts
+++ presenter/src/pages/optin/+title.ts
@@ -1,11 +1,1 @@
-// const title = pageContext.locale === 'de' ? 'DreamMall | Newsletter' : 'DreamMall | Newsletter'
 export const title = 'DreamMall | Newsletter'
-
-// import { PageContext } from 'vike/types'
-// export function title(pageContext: PageContext) {
-//   //   const title = pageContext.locale === 'de' ? 'DreamMall | Newsletter' : 'DreamMall | Newsletter'
-//   // eslint-disable-next-line no-console
-//   console.log('pageContext', pageContext)
-//   const title = 'DreamMall | Newsletter'
-//   return title
-// }
