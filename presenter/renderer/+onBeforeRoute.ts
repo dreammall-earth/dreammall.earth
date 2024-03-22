d34b5e81992611078c27c4018bf8f79cb8c70148
elweyn <heine.hannes@gmail.com>
2024-03-22 11:47:08 +0100

    Change pageContext.url to pageContext.urlLogical.

--- presenter/renderer/+onBeforeRoute.ts
+++ presenter/renderer/+onBeforeRoute.ts
@@ -1,21 +1,18 @@
 import { PageContext } from 'vike/types'
 
 import { extractLocale } from '#root/locales/extractLocale'
-import { useLocaleStore } from '#stores/localeStore'
 
 export function onBeforeRoute(pageContext: PageContext) {
-  if (pageContext.urlPathname.endsWith('.js')) return { pageContext: {} }
+  if (pageContext.urlPathname.endsWith('.js')) {
+    return { pageContext: { urlLogical: pageContext.urlPathname } }
+  }
+
   const { urlWithoutLocale, locale } = extractLocale(pageContext.urlPathname)
-  // eslint-disable-next-line no-console
-  console.log('urlWithoutLocale', urlWithoutLocale)
-  // eslint-disable-next-line no-console
-  console.log('locale', locale)
-  const localeStore = useLocaleStore()
-  localeStore.save(locale)
+
   return {
     pageContext: {
       // Make `locale` available as `pageContext.locale`
-      locale: locale,
+      locale,
       // Vike's router will use pageContext.urlLogical instead of pageContext.urlOriginal
       urlLogical: urlWithoutLocale,
     },
