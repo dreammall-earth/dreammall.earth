79faef18473e9c535c88d08297264879c7d817aa
elweyn <heine.hannes@gmail.com>
2024-03-25 10:38:24 +0100

    Test if changing language switch the i18n value.

--- presenter/src/components/language/LanguageSelector.test.ts
+++ presenter/src/components/language/LanguageSelector.test.ts
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect, beforeEach } from 'vitest'
 
+import i18n from '#plugins/i18n'
+
 import LanguageSelector from './LanguageSelector.vue'
 
 describe('LanguageSelector', () => {
@@ -10,12 +12,24 @@ describe('LanguageSelector', () => {
     })
   }
   let wrapper: ReturnType<typeof Wrapper>
+  let vSelect: ReturnType<typeof wrapper.findComponent>
 
   beforeEach(() => {
     wrapper = Wrapper()
+    vSelect = wrapper.findComponent({ name: 'v-select' })
   })
 
   it('renders', () => {
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  describe('switch locale with existing locale', () => {
+    beforeEach(async () => {
+      await vSelect.setValue('en')
+    })
+
+    it('change locale', () => {
+      expect(i18n.global.locale.value).toBe('en')
+    })
   })
 })
