67eebded175ad4e809318424adb304887593c534
elweyn <heine.hannes@gmail.com>
2024-03-26 12:38:26 +0100

    Remove check on defaultLocale in prerenderStart

--- presenter/renderer/+onPrerenderStart.ts
+++ presenter/renderer/+onPrerenderStart.ts
@@ -15,9 +15,7 @@ function onPrerenderStart(prerenderContext: { pageContexts: PageContextServer[] 
     locales.forEach((locale) => {
       // Localize URL
       let { urlOriginal } = pageContext
-      if (locale !== localeDefault) {
-        urlOriginal = `/${locale}${urlOriginal}`
-      }
+      urlOriginal = `/${locale}${urlOriginal}`
       storePageContexts.push({
         ...pageContext,
         urlOriginal,
