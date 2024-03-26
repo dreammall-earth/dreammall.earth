48b4a61d718009f14b5655945abc5153eb9d3f8a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-03-26 19:12:06 +0100

    missing changes

--- presenter/renderer/+onRenderHtml.ts
+++ presenter/renderer/+onRenderHtml.ts
@@ -1,7 +1,6 @@
 import { renderToString as renderToString_ } from '@vue/server-renderer'
 import { escapeInject, dangerouslySkipEscape } from 'vike/server'
 import { PageContext, PageContextServer } from 'vike/types'
-// import { resolveComponent } from 'vue'
 
 import logoUrl from '#assets/favicon.ico'
 import image from '#assets/img/dreammall-logo_social.png'
@@ -11,11 +10,6 @@ import { createApp } from './app'
 import { getDescription, getTitle } from './utils'
 
 import type { App } from 'vue'
-
-// this fixes a warning which occurs when building
-// > "resolveComponent" is imported from external module "vue" but never used in ...
-// running this here fixes the warning and should not impact anything due to prerender setting.
-// resolveComponent('nothing')
 
 async function render(pageContext: PageContextServer & PageContext) {
   const { app, i18n } = createApp(pageContext, false)
