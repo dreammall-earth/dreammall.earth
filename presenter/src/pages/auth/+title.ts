b25525a7fc4568cc7a427555ea51a6489ac5ec86
elweyn <heine.hannes@gmail.com>
2024-03-26 09:43:43 +0100

    Add translation of auth page title.

--- presenter/src/pages/auth/+title.ts
+++ presenter/src/pages/auth/+title.ts
@@ -1,1 +1,5 @@
-export const title = 'DreamMall | Authentifizierung'
+import i18n from '#plugins/i18n'
+
+export const title = () => {
+  return i18n.global.t('auth.title') // 'DreamMall | Authentifizierung'
+}
