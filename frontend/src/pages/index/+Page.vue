204ea8e1847ab0955c2952c630e3432b502d3ee7
Moriz Wahl <moriz.wahl@gmx.de>
2024-03-27 15:05:34 +0100

    purge index page

--- frontend/src/pages/index/+Page.vue
+++ frontend/src/pages/index/+Page.vue
@@ -1,16 +1,5 @@
 <template>
   <DefaultLayout>
-    <h1>{{ $t('home.h1') }}</h1>
-    <p>{{ $t('home.text1') }}</p>
-    <br />
-    <p>{{ $t('home.text2') }}</p>
-    <br />
-    <p>{{ $t('home.text3') }}</p>
-    <br />
-    <p>{{ $t('home.text4') }} <v-icon icon="mdi-heart" color="red" /></p>
-    <br />
-    <p>{{ $t('home.greet1') }}</p>
-    <p>{{ $t('home.greet2') }}</p>
   </DefaultLayout>
 </template>
 
