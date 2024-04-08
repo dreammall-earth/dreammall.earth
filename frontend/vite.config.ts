426d8abccd5255ceedcac0e6f16a1262fc2017f4
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-08 15:24:09 +0200

    no vike base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ const config: UserConfig = {
       vike({
         prerender: true,
         // baseAssets: 'https://stage1.dreammall.earth/app/',
-        baseAssets: '/app/',
+        // baseAssets: '/app/',
         // baseServer: '/app/',
       }), // SSR only when storybook is not running
     vueI18n({
