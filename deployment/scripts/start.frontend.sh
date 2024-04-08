f8f5900efa6b493530ee0ee5581d080d7ea2f16e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-08 14:24:47 +0200

    start frotnend script

---
+++ deployment/scripts/start.frontend.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Find current directory & configure paths
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+PROJECT_ROOT=$SCRIPT_DIR/../../frontend
+LOG_FILE=$SCRIPT_DIR/../../log/$(date +"%Y-%m-%d_%T")_pm2.frontend.log
+
+cd $PROJECT_ROOT
+pm2 start --name frontend "build/index.cjs" -l $LOG_FILE --log-date-format 'YYYY-MM-DD HH:mm:ss.SSS'
+pm2 save
