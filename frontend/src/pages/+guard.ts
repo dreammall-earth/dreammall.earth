37b394aab4fc19783bcb9304ad875a21f66838b9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-09 09:52:04 +0200

    fix guard

--- frontend/src/pages/+guard.ts
+++ frontend/src/pages/+guard.ts
@@ -6,14 +6,13 @@ import { useAuthStore } from '#stores/authStore'
 
 import type { GuardAsync } from 'vike/types'
 
-/*
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/require-await
 const guard: GuardAsync = async (pageContext): ReturnType<GuardAsync> => {
   const authStore = useAuthStore(pinia)
-  if (!authStore.isLoggedIn) {
-    throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
-  }
+  console.log(authStore.user, authStore.isLoggedIn)
+  // if (!authStore.isLoggedIn) {
+  //  throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
+  //}
 }
 
 export { guard }
-*/
