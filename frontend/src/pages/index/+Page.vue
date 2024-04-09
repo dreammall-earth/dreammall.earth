cd240f98e6758aa74c672c89066485a310565a0e
Moriz Wahl <moriz.wahl@gmx.de>
2024-04-09 12:50:09 +0200

    get query working

--- frontend/src/pages/index/+Page.vue
+++ frontend/src/pages/index/+Page.vue
@@ -8,22 +8,32 @@
           label="To Room"
           size="auto"
           @click="enterRoom"
-        >{{ $t('buttons.signout') }}</v-btn>
+          >{{ $t('buttons.signout') }}</v-btn
+        >
       </v-col>
     </v-row>
   </DefaultLayout>
 </template>
 
 <script lang="ts" setup>
- import { DefaultApolloClient, useQuery } from '@vue/apollo-composable'
- import { ApolloClient, InMemoryCache } from '@apollo/client/core'
- import { querySecret } from '#queries/querySecret'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { inject } from 'vue'
 
- 
- import DefaultLayout from '#layouts/DefaultLayout.vue'
- 
- 
- const enterRoom = () => {
-   console.log(`Redirect to ${}`)
- }
+import DefaultLayout from '#layouts/DefaultLayout.vue'
+// eslint-disable-next-line import/no-relative-parent-imports
+import { getRoom } from '#queries/getRoom'
+
+const apolloClient = inject<ApolloClient<InMemoryCache>>(DefaultApolloClient)
+
+const enterRoom = async () => {
+  try {
+    const result = await apolloClient?.query({ query: getRoom })
+    // eslint-disable-next-line no-console
+    console.log(`Redirect to ${result?.data?.getRoom}`)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('error secret', error)
+  }
+}
 </script>
