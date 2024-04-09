106607e9e3a81e4811eb90ba2e10ff76147acd16
Moriz Wahl <moriz.wahl@gmx.de>
2024-04-09 12:28:19 +0200

    path to queries

--- frontend/src/pages/index/+Page.vue
+++ frontend/src/pages/index/+Page.vue
@@ -1,7 +1,29 @@
 <template>
-  <DefaultLayout> </DefaultLayout>
+  <DefaultLayout>
+    <v-row>
+      <v-col>
+        <v-btn
+          class="room-button"
+          variants="outlined"
+          label="To Room"
+          size="auto"
+          @click="enterRoom"
+        >{{ $t('buttons.signout') }}</v-btn>
+      </v-col>
+    </v-row>
+  </DefaultLayout>
 </template>
 
 <script lang="ts" setup>
-import DefaultLayout from '#layouts/DefaultLayout.vue'
+ import { DefaultApolloClient, useQuery } from '@vue/apollo-composable'
+ import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+ import { querySecret } from '#queries/querySecret'
+
+ 
+ import DefaultLayout from '#layouts/DefaultLayout.vue'
+ 
+ 
+ const enterRoom = () => {
+   console.log(`Redirect to ${}`)
+ }
 </script>
