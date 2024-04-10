52d20f7c79396a05a857879fbe397bf6c722c0ff
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-10 12:08:43 +0200

    disable guard

--- frontend/src/pages/+guard.ts
+++ frontend/src/pages/+guard.ts
@@ -5,8 +5,9 @@ import { AUTH } from '#src/env'
 import type { GuardSync } from 'vike/types'
 
 const guard: GuardSync = (pageContext): ReturnType<GuardSync> => {
+  console.log('guard', pageContext.hasToken)
   if (!pageContext.hasToken) {
-    throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
+    // throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
   }
 }
 
