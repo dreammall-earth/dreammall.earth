fe451f86840fc7c0131ba3e41709d379a62aef6d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-10 12:23:13 +0200

    fix guard for client side rendering

--- frontend/src/pages/+guard.ts
+++ frontend/src/pages/+guard.ts
@@ -1,13 +1,17 @@
 import { redirect } from 'vike/abort'
 
+import pinia from '#plugins/pinia'
 import { AUTH } from '#src/env'
+import { useAuthStore } from '#stores/authStore'
 
 import type { GuardSync } from 'vike/types'
 
 const guard: GuardSync = (pageContext): ReturnType<GuardSync> => {
-  console.log('guard', pageContext.hasToken)
-  if (!pageContext.hasToken) {
-    // throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
+  // the store is the client side part, pageContext the serverSide part
+  // there might be a better solution to combine both into pageContext
+  const authStore = useAuthStore(pinia)
+  if (!pageContext.hasToken && !authStore.isLoggedIn) {
+    throw redirect(AUTH.UNAUTHORIZED_REDIRECT_URI)
   }
 }
 
