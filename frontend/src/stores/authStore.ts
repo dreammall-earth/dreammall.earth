9b1a040149656fd3019e3997adb2d70600912ce9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-11 10:28:41 +0200

    fix isLoggedIn computed

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -8,7 +8,9 @@ export const useAuthStore = defineStore(
     const user = ref<User | null>(null)
 
     const accessToken = computed(() => user.value?.access_token ?? '')
-    const isLoggedIn = computed(() => !!user.value)
+    const isLoggedIn = computed(
+      () => !!user.value && user.value.expires_at && user.value.expires_at >= new Date().valueOf(),
+    )
 
     const save = (u: User | null) => {
       user.value = u
