4e7783cb823248598dbb9dcb8b990feb1d32a658
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-16 11:27:45 +0200

    also log response

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -1,6 +1,6 @@
 import { randomBytes } from 'crypto'
 
-import { GraphQLRequestContext } from '@apollo/server'
+import { GraphQLRequestContext, GraphQLRequestContextWillSendResponse } from '@apollo/server'
 
 import logger from '#src/logger'
 
@@ -11,18 +11,28 @@ export default {
   // eslint-disable-next-line @typescript-eslint/require-await
   async requestDidStart(requestContext: GraphQLRequestContext<Context>) {
     const qID = randomBytes(4).toString('hex')
-    const logElements = ['Apollo Request', qID, requestContext.request.operationName]
+    const logRequest = ['Apollo Request', qID, requestContext.request.operationName]
     if (requestContext.request.operationName !== 'IntrospectionQuery') {
-      logElements.push(JSON.stringify(requestContext.request.query))
+      logRequest.push(JSON.stringify(requestContext.request.query))
     }
     if (requestContext.request.variables) {
-      logElements.push(JSON.stringify(requestContext.request.variables))
+      logRequest.push(JSON.stringify(requestContext.request.variables))
     }
-    logger.debug(...logElements)
+    logger.debug(...logRequest)
     return {
       // eslint-disable-next-line @typescript-eslint/require-await
-      async willSendResponse() {
-        logger.debug('Apollo Response', qID)
+      async willSendResponse(requestContext: GraphQLRequestContextWillSendResponse<Context>) {
+        const logResponse = ['Apollo Response', qID]
+        if (requestContext.errors) {
+          logger.error(...logResponse, JSON.stringify(requestContext.errors))
+          return
+        }
+
+        if (requestContext.request.operationName !== 'IntrospectionQuery') {
+          logResponse.push(JSON.stringify(requestContext.response.body))
+        }
+
+        logger.debug(...logResponse)
       },
     }
   },
