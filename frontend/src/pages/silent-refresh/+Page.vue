ee894a7eae198a4f44879623a1cda48e3e664a7e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-16 12:42:07 +0200

    fix lint

--- frontend/src/pages/silent-refresh/+Page.vue
+++ frontend/src/pages/silent-refresh/+Page.vue
@@ -13,7 +13,6 @@ import AuthService from '#src/services/AuthService'
 
 const authService = inject<AuthService>('authService')
 
-
 onBeforeMount(async () => {
   try {
     await authService?.renewToken()
