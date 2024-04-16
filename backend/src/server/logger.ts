d636b1ccd5cfb435c3b5d368b43882f1125ed5bc
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-04-16 10:32:02 +0200

    fix logged plugin

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -1,21 +1,17 @@
-import {
-  GraphQLRequest,
-  GraphQLRequestContext,
-  GraphQLRequestContextWillSendResponse,
-} from '@apollo/server'
+import { GraphQLRequestContext, GraphQLRequestContextWillSendResponse } from '@apollo/server'
 
 import logger from '#src/logger'
+
+import { Context } from './context'
 
 export default {
   // Fires whenever a GraphQL request is received from a client.
   // eslint-disable-next-line @typescript-eslint/require-await
-  async requestDidStart(requestContext: GraphQLRequestContext<GraphQLRequest>) {
+  async requestDidStart(requestContext: GraphQLRequestContext<Context>) {
     logger.debug('Apollo Request', requestContext.request)
     return {
       // eslint-disable-next-line @typescript-eslint/require-await
-      async willSendResponse(
-        requestContext: GraphQLRequestContextWillSendResponse<GraphQLRequest>,
-      ) {
+      async willSendResponse(requestContext: GraphQLRequestContextWillSendResponse<Context>) {
         logger.debug('Apollo Response', requestContext.request, requestContext.response.body)
       },
     }
