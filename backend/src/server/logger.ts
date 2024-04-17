16a402223654dc8ef2f5ecc7ea09a579ef79af2d
Moriz Wahl <moriz.wahl@gmx.de>
2024-04-17 15:02:50 +0200

    do not collect logger info when not logging

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -12,23 +12,26 @@ export default {
   async requestDidStart(requestContext: GraphQLRequestContext<Context>) {
     const isIntrospectionQuery = requestContext.request.operationName === 'IntrospectionQuery'
     const qID = randomBytes(4).toString('hex')
-    const logRequest = ['Apollo Request', qID, requestContext.request.operationName]
-    logRequest.push(JSON.stringify(requestContext.request.query))
-    if (requestContext.request.variables) {
-      logRequest.push(JSON.stringify(requestContext.request.variables))
+    if (!isIntrospectionQuery) {
+      const logRequest = ['Apollo Request', qID, requestContext.request.operationName]
+      logRequest.push(JSON.stringify(requestContext.request.query))
+      if (requestContext.request.variables) {
+        logRequest.push(JSON.stringify(requestContext.request.variables))
+      }
+      logger.debug(...logRequest)
     }
-    if (!isIntrospectionQuery) logger.debug(...logRequest)
     return {
       // eslint-disable-next-line @typescript-eslint/require-await
       async willSendResponse(requestContext: GraphQLRequestContextWillSendResponse<Context>) {
-        const logResponse = ['Apollo Response', qID]
-        if (requestContext.errors) {
-          logger.error(...logResponse, JSON.stringify(requestContext.errors))
-          return
+        if (!isIntrospectionQuery) {
+          const logResponse = ['Apollo Response', qID]
+          if (requestContext.errors) {
+            logger.error(...logResponse, JSON.stringify(requestContext.errors))
+            return
+          }
+          logResponse.push(JSON.stringify(requestContext.response.body))
+          logger.debug(...logResponse)
         }
-
-        logResponse.push(JSON.stringify(requestContext.response.body))
-        if (!isIntrospectionQuery) logger.debug(...logResponse)
       },
     }
   },
