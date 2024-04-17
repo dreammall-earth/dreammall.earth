ef2b759a8e1f66898c9a4e42f8dad2ffbcfa5425
Moriz Wahl <moriz.wahl@gmx.de>
2024-04-17 14:04:07 +0200

    fix(backend): no introspection query in log

--- backend/src/server/logger.ts
+++ backend/src/server/logger.ts
@@ -10,15 +10,14 @@ export default {
   // Fires whenever a GraphQL request is received from a client.
   // eslint-disable-next-line @typescript-eslint/require-await
   async requestDidStart(requestContext: GraphQLRequestContext<Context>) {
+    const isIntrospectionQuery = requestContext.request.operationName === 'IntrospectionQuery'
     const qID = randomBytes(4).toString('hex')
     const logRequest = ['Apollo Request', qID, requestContext.request.operationName]
-    if (requestContext.request.operationName !== 'IntrospectionQuery') {
-      logRequest.push(JSON.stringify(requestContext.request.query))
-    }
+    logRequest.push(JSON.stringify(requestContext.request.query))
     if (requestContext.request.variables) {
       logRequest.push(JSON.stringify(requestContext.request.variables))
     }
-    logger.debug(...logRequest)
+    if (!isIntrospectionQuery) logger.debug(...logRequest)
     return {
       // eslint-disable-next-line @typescript-eslint/require-await
       async willSendResponse(requestContext: GraphQLRequestContextWillSendResponse<Context>) {
@@ -28,11 +27,8 @@ export default {
           return
         }
 
-        if (requestContext.request.operationName !== 'IntrospectionQuery') {
-          logResponse.push(JSON.stringify(requestContext.response.body))
-        }
-
-        logger.debug(...logResponse)
+        logResponse.push(JSON.stringify(requestContext.response.body))
+        if (!isIntrospectionQuery) logger.debug(...logResponse)
       },
     }
   },
