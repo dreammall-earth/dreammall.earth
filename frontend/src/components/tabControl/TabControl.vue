a46766247e25a7b26c624333df3825b51db8518b
Maximilian Harz <maxharz@gmail.com>
2024-05-02 13:38:07 +0200

    Style tab control for header (WIP)

--- frontend/src/components/tabControl/TabControl.vue
+++ frontend/src/components/tabControl/TabControl.vue
@@ -1,13 +1,16 @@
 <template>
   <button class="tabControl" :class="{ open: isOpen }" @click="toggleOpen">
     <button class="item" :class="{ active: activeItem === 1 }" @click="activeItem = 1">
-      <v-img class="w-100" :src="WorldCafeIcon" /> Weltencafe
+      <div class="icon"><v-img class="w-100" :src="WorldCafeIcon" /></div>
+      Weltencafe
     </button>
     <button class="item" :class="{ active: activeItem === 2 }" @click="activeItem = 2">
-      <v-img class="w-100" :src="MallIcon" />Mall
+      <div class="icon"><v-img class="w-100" :src="MallIcon" /></div>
+      Mall
     </button>
     <button class="item" :class="{ active: activeItem === 3 }" @click="activeItem = 3">
-      <v-img class="w-100" :src="CockpitIcon" />Cockpit
+      <div class="icon"><v-img class="w-100" :src="CockpitIcon" /></div>
+      Cockpit
     </button>
   </button>
 </template>
@@ -29,17 +32,67 @@ const activeItem = ref(1)
 </script>
 
 <style scoped lang="scss">
+.icon {
+  display: flex;
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  margin-right: 2px;
+  transition:
+    margin-right var(--animation-time),
+    background var(--animation-time);
+}
+
 .tabControl {
-  width: 400px; // TODO calculate width
+  --animation-time: 0.3s;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 10px;
   border-radius: 27.067px;
   border: 0.677px solid rgba(255, 255, 255, 0.78); // TODO use color variable
   background: #e1e6ed; // TODO use color variable
 
-  &:not(.open) .item:not(.active) {
-    display: none;
+  &.open {
+    .item {
+      display: flex;
+      &.active {
+        border-radius: 27.067px;
+        border: 0.677px solid rgba(255, 255, 255, 0.78);
+        background: #fff;
+
+        .icon {
+          background: #e1e6ed;
+          border-radius: 50%;
+        }
+      }
+    }
+  }
+
+  &:not(.open) {
+    .item {
+      display: flex;
+    }
+    .item:not(.active) {
+      display: none;
+    }
+  }
+}
+
+.item {
+  align-items: center;
+  padding: 10px;
+  width: fit-content; // TODO calculate width
+  &.active {
+    border-radius: 27.067px;
+    border: 0.677px solid rgba(255, 255, 255, 0.78);
+    background: #fff;
+
+    .icon {
+      background: #e1e6ed;
+      border-radius: 50%;
+      margin-right: 5px;
+    }
   }
 }
 </style>
