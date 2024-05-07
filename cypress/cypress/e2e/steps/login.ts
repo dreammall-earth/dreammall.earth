d75830e155d76c764e7b9f056055933432702fe7
mahula <lenzmath@posteo.de>
2024-05-07 19:46:20 +0200

    use origin method for redirected flow in e2e test workflow

--- cypress/cypress/e2e/steps/login.ts
+++ cypress/cypress/e2e/steps/login.ts
@@ -8,22 +8,40 @@ import 'cypress-network-idle'
 import { loginPage } from '../pages/LoginPage'
 import { worldcafePage } from '../pages/WorldcafePage'
 
-Given('The browser navigates to the login page', () => {
-  cy.visit('/signin')
-  cy.waitForNetworkIdle(5000)
-  loginPage.usernameFieldIsVisible()
-})
+// Given('The browser navigates to the login page', () => {
+//   // signin langing page redirects to Authentik signin
+//   cy.visit('/signin')
+//   cy.waitForNetworkIdle(5000)
+//   // loginPage.usernameFieldIsVisible()
+// })
 
 When('I submit the credentials {string} {string}', (username: string, password: string) => {
-  loginPage.submitUsername(username)
+  cy.visit('/signin')
   cy.waitForNetworkIdle(5000)
-  loginPage.submitPassword(password)
+  // loginPage.usernameFieldIsVisible()
+
+  cy.origin(
+    'http://localhost:9000/if/flow/dreammallearth-authentication-flow/',
+    { args: { username, password } },
+    ({ username, password }) => {
+      // loginPage.submitUsername(username)
+      cy.get('input[name="uidField"]').type(username)
+      cy.get('button[type="submit"]').click()
+      // cy.waitForNetworkIdle(5000)
+      cy.wait(5000)
+      // loginPage.submitPassword(password)
+      cy.get('input[name="password"]').type(password)
+      cy.get('button[type="submit"]').click()
+      cy.url({ timeout: 8000 }).should('include', 'authorization-implicit-consent')
+      // loginPage.agreeConsent()
+      cy.get('button[type="submit"]').click()
+    },
+  )
 })
 
-When('I agree to the consent', () => {
-  cy.url({ timeout: 8000 }).should('include', 'authorization-implicit-consent')
-  loginPage.agreeConsent()
-})
+// When('I agree to the consent', () => {
+//   loginPage.agreeConsent()
+// })
 
 Then('I am on the worldcafe page', () => {
   cy.waitForNetworkIdle(20000)
