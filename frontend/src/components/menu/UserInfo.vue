18cfbf77f64a8974b9268208e37a3ddbde47a291
Maximilian Harz <maxharz@gmail.com>
2024-05-15 16:22:07 +0200

    Add preliminary dropdown with logout

--- frontend/src/components/menu/UserInfo.vue
+++ frontend/src/components/menu/UserInfo.vue
@@ -1,19 +1,36 @@
 <template>
-  <div
-    class="user-info rounded-pill d-flex flex-row text-icon bg-icon-background border-sm align-center justify-center"
-  >
-    <v-avatar class="avatar d-flex align-center text-font border-sm bg-primary" size="44">
-      <v-img v-if="userImage" :src="userImage" />
-      <span v-else>{{ initals?.toUpperCase() }}</span>
-    </v-avatar>
-    <div class="d-flex flex-column justify-center text-right pa-1 pl-3 w-100">
-      <v-icon icon="$ellipsis"></v-icon>
-    </div>
-  </div>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <button
+        v-bind="props"
+        class="user-info rounded-pill d-flex flex-row text-icon bg-icon-background border-sm align-center justify-center"
+      >
+        <v-avatar class="avatar d-flex align-center text-font border-sm bg-primary" size="44">
+          <v-img v-if="userImage" :src="userImage" />
+          <span v-else>{{ initals?.toUpperCase() }}</span>
+        </v-avatar>
+        <div class="d-flex flex-column justify-center text-right pa-1 pl-3 w-100">
+          <v-icon icon="$ellipsis"></v-icon>
+        </div>
+      </button>
+    </template>
+    <MainButton
+      v-if="auth.isLoggedIn"
+      class="sign-out mr-4"
+      variant="third"
+      label="Sign Out"
+      size="auto"
+      @click="signOut"
+      >{{ $t('buttons.signout') }}
+    </MainButton>
+  </v-menu>
 </template>
 
 <script lang="ts" setup>
+import { inject } from 'vue'
 import { useAuthStore } from '#stores/authStore'
+import AuthService from '#src/services/AuthService'
+import MainButton from '#components/buttons/MainButton.vue'
 
 // Avoid problems when same svg is used several times by randomizing path and image ids
 const id = Math.floor(Math.random() * 10e4)
@@ -26,6 +43,19 @@ const initals = name
   .map((n) => n.charAt(0))
   .join('')
 const userImage = authStore.user?.profile.picture
+
+const authService = inject<AuthService>('authService')
+
+const auth = useAuthStore()
+
+async function signOut() {
+  try {
+    await authService?.signOut()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('auth error', error)
+  }
+}
 </script>
 
 <style scoped lang="scss">
