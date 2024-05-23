23c822df5b912affab4706a5a22360d5310bcc0d
elweyn <heine.hannes@gmail.com>
2024-05-23 10:45:26 +0200

    Add CreateButton to the Frontend index page.

--- frontend/src/pages/index/+Page.vue
+++ frontend/src/pages/index/+Page.vue
@@ -1,37 +1,13 @@
 <template>
   <DefaultLayout>
-    <h1 class="d-flex justify-center">{{ $t('cafe.welcome') }}</h1>
-    <v-row>
-      <v-col>
-        <MainButton
-          class="room-button"
-          variant="fourth"
-          label="To Room"
-          size="auto"
-          @click="enterRoom"
-          >{{ $t('buttons.toRoom') }}</MainButton
-        >
-      </v-col>
-    </v-row>
-    <v-row v-if="auth.isAdmin">
-      <v-col>
-        <MainButton
-          class="admin-button"
-          variant="primary"
-          label="To Admin"
-          size="auto"
-          @click="enterAdmin"
-          >{{ $t('buttons.toAdmin') }}</MainButton
-        >
-      </v-col>
-    </v-row>
+    <create-button />
   </DefaultLayout>
 </template>
 
 <script lang="ts" setup>
 import { useQuery } from '@vue/apollo-composable'
 
-import MainButton from '#components/buttons/MainButton.vue'
+import CreateButton from '#components/buttons/CreateButton.vue'
 import DefaultLayout from '#layouts/DefaultLayout.vue'
 import { joinMyRoomQuery } from '#queries/joinMyRoomQuery'
 import { AUTH } from '#src/env'
