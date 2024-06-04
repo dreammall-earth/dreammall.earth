63ab1087080739e127b1d0ded6d4246f7bfc0d77
mahula <lenzmath@posteo.de>
2024-06-04 21:23:10 +0200

    refine e2e test reporting configuration

--- cypress/cypress/create-cucumber-html-report.ts
+++ cypress/cypress/create-cucumber-html-report.ts
@@ -1,11 +1,11 @@
 import report from 'multiple-cucumber-html-reporter'
 
-const reportTitle = 'End-to-End Test Report'
+const reportTitle = 'DreamMall End-to-End Test Report'
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 report.generate({
-  jsonDir: './reports/json/',
-  reportPath: './reports/e2e_html_report',
+  jsonDir: './reports/',
+  reportPath: './reports/dreammall-e2e_html_report',
   pageTitle: reportTitle,
   reportName: reportTitle,
   pageFooter: '<div></div>',
