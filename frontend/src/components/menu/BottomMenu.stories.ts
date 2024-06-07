4ac9a96ca99153350242735deac1bf0e8613dc6d
ogerly <ogerly@tutanota.com>
2024-06-07 11:37:02 +0200

    change button-group to v-bottom-navigation

--- frontend/src/components/menu/BottomMenu.stories.ts
+++ frontend/src/components/menu/BottomMenu.stories.ts
@@ -5,7 +5,7 @@ import BottomMenu from './BottomMenu.vue'
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 const meta = {
-  title: 'Menu/BottomMenu',
+  title: 'ORGANISMS/BottomMenu',
   component: BottomMenu as SBComp,
   tags: ['autodocs'],
   argTypes: {},
--- frontend/src/components/menu/BottomMenu.vue
+++ frontend/src/components/menu/BottomMenu.vue
@@ -1,28 +1,61 @@
 <template>
-  <div
+  <div>
+  <!-- <div
     class="bottom-menu w-100 position-fixed bottom-0 justify-space-around align-center py-2 bg-surface"
   >
     <MessageIndicator :number-of-messages="3" />
     <NewsIndicator :has-news="true" />
-    <Circle>
+    <Circle  @click="toggleDrawer">
       <v-icon icon="$camera"></v-icon>
     </Circle>
-    <UserInfo />
-  </div>
+    <UserInfo />    
+  </div> -->
+  
+
+ 
+<v-bottom-navigation>
+  <v-btn value="message">
+    <MessageIndicator :number-of-messages="3" />
+  </v-btn>
+
+  <v-btn value="news">
+    <NewsIndicator :has-news="true" />
+  </v-btn>
+
+  <v-btn value="rooms">
+    <Circle  @click="toggleDrawer">
+      <v-icon icon="$camera"></v-icon>
+    </Circle>
+  </v-btn>
+  <v-btn value="rooms">
+    <UserInfo /> 
+  </v-btn>
+ 
+</v-bottom-navigation>
+<ListWithNavigationDrawer :drawer="drawer" @update:drawer="drawer = $event" :location="location" />
+</div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
 import Circle from './CircleElement.vue'
 import MessageIndicator from './MessageIndicator.vue'
 import NewsIndicator from './NewsIndicator.vue'
 import UserInfo from './UserInfo.vue'
+import ListWithNavigationDrawer from './ListWithNavigationDrawer.vue'
+
+
+const drawer = ref(false)
+
+const toggleDrawer = () => {
+  drawer.value = !drawer.value
+}
+
+const location = ref<'bottom' | 'right' | 'left' | 'end' | 'top' | 'start'>('bottom') // Beispiel für dynamische Anpassung
 </script>
 
 <style scoped lang="scss">
-.bottom-menu {
-  bottom: 0;
-  background: var(--v-bottom-menu-background) !important;
-  backdrop-filter: blur(20px);
-  border-radius: 30px 30px 0 0;
+ListWithNavigationDrawer {
+  z-index: 10;
 }
 </style>
