da28dac53ba88fb0b8c02e0ab0132a9180fa83b5
Maximilian Harz <maxharz@gmail.com>
2024-06-12 17:11:57 +0200

    Mock apollo client in Storybook, add story for room page (not functional)

---
+++ frontend/.storybook/ApolloWrapper.vue
@@ -0,0 +1,33 @@
+<template>
+  <div><slot /></div>
+</template>
+
+<script>
+import { defineComponent, provide } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+import { joinMyRoomQuery } from '#queries/joinMyRoomQuery'
+import { MockedProvider } from '@apollo/client/testing'
+
+const apolloClient = new MockedProvider({
+  mocks: [
+    {
+      request: {
+        query: joinMyRoomQuery,
+      },
+      result: {
+        data: {
+          joinMyRoom: 'https://meet.jit.si/room',
+        },
+        joinMyRoom: 'https://meet.jit.si/room',
+      },
+    },
+  ],
+})
+
+export default defineComponent({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+})
+</script>
