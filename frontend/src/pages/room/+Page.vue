d3149ec7ea40e2c4796a3a6f680f5d9420daa371
elweyn <heine.hannes@gmail.com>
2024-06-13 22:25:32 +0200

    Move query roomUrl to room page.

--- frontend/src/pages/room/+Page.vue
+++ frontend/src/pages/room/+Page.vue
@@ -1,17 +1,41 @@
 <template>
   <DefaultLayout>
     <div class="container">
-      <EmbeddedRoom :url="roomUrl" />
+      <EmbeddedRoom :url="roomUrl.value" />
     </div>
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
-import EmbeddedRoom from '#components/embedded-room/EmbeddedRoom.vue'
-import useMyRoom from '#components/embedded-room/useMyRoom'
-import DefaultLayout from '#layouts/DefaultLayout.vue'
+import { useQuery } from '@vue/apollo-composable'
+import { ref, watch } from 'vue'
 
-const { roomUrl } = useMyRoom()
+import EmbeddedRoom from '#components/embedded-room/EmbeddedRoom.vue'
+import DefaultLayout from '#layouts/DefaultLayout.vue'
+import { JoinMyRoomQueryResult, joinMyRoomQuery } from '#queries/joinMyRoomQuery.js'
+
+// const { roomUrl } = useMyRoom()
+
+const { result: joinMyRoomQueryResult, error: joinMyRoomQueryError } =
+  useQuery<JoinMyRoomQueryResult>(joinMyRoomQuery, null, {
+    prefetch: false,
+    fetchPolicy: 'no-cache',
+  })
+
+const roomUrl = ref<string | null>(null)
+
+watch(joinMyRoomQueryResult, () => {
+  if (joinMyRoomQueryResult.value) {
+    roomUrl.value = joinMyRoomQueryResult.value.joinMyRoom
+  }
+})
+
+watch(joinMyRoomQueryError, () => {
+  if (joinMyRoomQueryError.value) {
+    // eslint-disable-next-line no-console
+    console.log(joinMyRoomQueryError.value.message)
+  }
+})
 </script>
 
 <style scoped>
