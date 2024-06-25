26fda6889652582131b51f4cd36080b0acd55ca1
Moriz Wahl <moriz.wahl@gmx.de>
2024-06-25 10:02:46 +0200

    soemthing

--- frontend/src/pages/room/+Page.vue
+++ frontend/src/pages/room/+Page.vue
@@ -11,7 +11,7 @@ import EmbeddedRoom from '#components/embedded-room/EmbeddedRoom.vue'
 import useMyRoom from '#components/embedded-room/useMyRoom'
 import DefaultLayout from '#layouts/DefaultLayout.vue'
 
-const { roomUrl } = await useMyRoom()
+const { roomUrl } = useMyRoom()
 </script>
 
 <style scoped>
