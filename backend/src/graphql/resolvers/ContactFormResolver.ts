1426a39465db2b0ad7766abfd172860f9b23be07
Moriz Wahl <moriz.wahl@gmx.de>
2024-06-26 11:25:41 +0200

    async function

--- backend/src/graphql/resolvers/ContactFormResolver.ts
+++ backend/src/graphql/resolvers/ContactFormResolver.ts
@@ -7,7 +7,9 @@ import { EVENT_CONTACTFORM_SEND } from '#src/event/Events'
 @Resolver()
 export class ContactFormResolver {
   @Mutation(() => Boolean)
-  createContactForm(@Arg('contactFormData') contactFormData: ContactFormInput): boolean {
+  async createContactForm(
+    @Arg('contactFormData') contactFormData: ContactFormInput,
+  ): Promise<boolean> {
     void sendContactEmails(contactFormData)
     await EVENT_CONTACTFORM_SEND(contactFormData.email)
     return true
