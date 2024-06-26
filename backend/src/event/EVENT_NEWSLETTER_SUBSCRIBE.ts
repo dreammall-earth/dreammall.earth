7cab169f18463891a89f860c68683f1a677d5b2e
Moriz Wahl <moriz.wahl@gmx.de>
2024-06-26 12:44:33 +0200

    event async await

--- backend/src/event/EVENT_NEWSLETTER_SUBSCRIBE.ts
+++ backend/src/event/EVENT_NEWSLETTER_SUBSCRIBE.ts
@@ -1,5 +1,5 @@
 import { Event } from './Event'
 import { EventType } from './EventType'
 
-export const EVENT_NEWSLETTER_SUBSCRIBE = (involvedEmail: string) =>
-  Event(EventType.NEWSLETTER_SUBSCRIBE, { involvedEmail })
+export const EVENT_NEWSLETTER_SUBSCRIBE = async (involvedEmail: string) =>
+  await Event(EventType.NEWSLETTER_SUBSCRIBE, { involvedEmail })
