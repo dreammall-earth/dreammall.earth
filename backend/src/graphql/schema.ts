e6de315eede4eacd1bc50b082bf5469b959a00de
Moriz Wahl <moriz.wahl@gmx.de>
2024-06-27 04:49:34 +0200

    simple test subscription

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -3,6 +3,7 @@ import { buildSchema } from 'type-graphql'
 
 import { authChecker } from '#src/auth/authChecker'
 
+import { pubSub } from './pubSub'
 import { ContactFormResolver } from './resolvers/ContactFormResolver'
 import { NewsletterSubscriptionResolver } from './resolvers/NewsletterSubscriptionResolver'
 import { RoomResolver } from './resolvers/RoomResolver'
@@ -10,6 +11,7 @@ import { RoomResolver } from './resolvers/RoomResolver'
 export const schema = async (): Promise<GraphQLSchema> => {
   return buildSchema({
     resolvers: [ContactFormResolver, NewsletterSubscriptionResolver, RoomResolver],
+    pubSub,
     validate: {
       validationError: { target: false },
       skipMissingProperties: true,
