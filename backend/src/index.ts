c5f5a84e89173b01722f62bc3ab78d7c30f6cf73
Moriz Wahl <moriz.wahl@gmx.de>
2024-06-27 15:33:30 +0200

    move handle open rooms to schema, define config for bbb pull meetings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,13 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata'
-import { CONFIG } from '#config/config'
-import { handleOpenRooms } from '#graphql/resolvers/dal/handleOpenRooms'
 
 import logger from './logger'
 import { prisma } from './prisma'
 import { listen } from './server/server'
 
-const checkForOpenRooms = (): void => {
-  void handleOpenRooms()
-  setTimeout(checkForOpenRooms, 60 * 1000)
-}
-
 export const main = async (): Promise<void> => {
   const url = await listen(4000)
   logger.info(`🚀 Server is ready at ${url}`)
-  // if (CONFIG.BBB_URL) setTimeout(checkForOpenRooms, 60 * 1000)
 }
 
 void main()
