1dffc46ad60fe247933cdeb75d8cb76e47e00c7c
elweyn <heine.hannes@gmail.com>
2024-06-27 12:27:06 +0200

    Remove checkForOpenRooms.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,20 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata'
-import { handleOpenRooms } from '#graphql/resolvers/dal/handleOpenRooms'
+// import { handleOpenRooms } from '#graphql/resolvers/dal/handleOpenRooms'
 
 import logger from './logger'
 import { prisma } from './prisma'
 import { listen } from './server/server'
 
-const checkForOpenRooms = (): void => {
-  void handleOpenRooms()
-  setTimeout(checkForOpenRooms, 60 * 1000)
-}
+// const checkForOpenRooms = (): void => {
+//   void handleOpenRooms()
+//   setTimeout(checkForOpenRooms, 60 * 1000)
+// }
 
 export const main = async (): Promise<void> => {
   const url = await listen(4000)
   logger.info(`🚀 Server is ready at ${url}`)
-  checkForOpenRooms()
+  // checkForOpenRooms()
 }
 
 void main()
