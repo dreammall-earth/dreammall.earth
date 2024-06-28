6bbd74473dd35a2933ff17c9f411f7e6c63302ba
ogerly <ogerly@tutanota.com>
2024-06-28 08:40:11 +0200

    add ListWithNavigationDrawer in bottom menu and topmenu

--- frontend/src/components/menu/BottomMenu.vue
+++ frontend/src/components/menu/BottomMenu.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="navigation-drawer-box d-md-none d-lg-none position-fixed mb-5 pb-5">
-    <v-navigation-drawer
+    <!-- <v-navigation-drawer
       :model-value="drawer"
       :location="location"
       width="300px"
+      overlay
       @update:model-value="updateDrawer"
     >
       <v-list>
         <div class="mx-4">{{ $t('menu.roomList') }}</div>
         <ListElement :items="items" />
       </v-list>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
+    <ListWithNavigationDrawer :drawer="drawer" :location="location" @update:drawer="updateDrawer" />
   </div>
   <div
     class="bottom-menu d-flex w-100 position-fixed bottom-0 justify-space-around align-center py-2 bg-surface d-md-none d-lg-none"
@@ -39,6 +41,8 @@ import UserInfo from './UserInfo.vue'
 
 import MessageIndicator from '#components/menu/MessageIndicator.vue'
 import NewsIndicator from '#components/menu/NewsIndicator.vue'
+import ListWithNavigationDrawer from '#components/vuetify/Organisms/ListWithNavigationDrawer.vue'
+
 
 const roomsStore = useRoomsStore()
 const { rooms: items } = storeToRefs(roomsStore)
@@ -54,7 +58,6 @@ const updateDrawer = (value: boolean) => {
   drawer.value = value
 }
 
-// const emits = defineEmits(['update:drawer'])
 </script>
 
 <style scoped lang="scss">
