a1f76a0ff7c89ccda456e1bb7163662dc398d461
Maximilian Harz <maxharz@gmail.com>
2024-07-03 19:06:59 +0200

    Add AboutMe (WIP)

---
+++ frontend/src/components/cockpit/about-me/AboutMe.vue
@@ -0,0 +1,86 @@
+<template>
+  <CockpitCard>
+    <template #header>
+      <h2>{{ $t('cockpit.about-me.header') }}</h2>
+    </template>
+    <template #default>
+      <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
+      <div class="about-me-text">"{{ user.text }}"</div>
+      <hr />
+      <ul class="details">
+        <li v-if="user.place"><v-icon icon="$place"></v-icon> {{ user.place }}</li>
+        <li v-if="user.work"><v-icon icon="$working"></v-icon> {{ user.work }}</li>
+        <li></li>
+      </ul>
+      <ul class="social">
+        <li v-if="user.social.facebook">
+          <a :href="user.social.facebook" target="_blank" rel="noopener noreferrer">
+            <v-icon icon="$facebook"></v-icon>
+          </a>
+        </li>
+        <li v-if="user.social.linkedin">
+          <a :href="user.social.linkedin" target="_blank" rel="noopener noreferrer">
+            <v-icon icon="$linkedIn"></v-icon>
+          </a>
+        </li>
+        <li v-if="user.social.xing">
+          <a :href="user.social.xing" target="_blank" rel="noopener noreferrer">
+            <v-icon icon="$xing"></v-icon>
+          </a>
+        </li>
+        <li v-if="user.social.x">
+          <a :href="user.social.x" target="_blank" rel="noopener noreferrer">
+            <v-icon icon="$x"></v-icon>
+          </a>
+        </li>
+      </ul>
+    </template>
+  </CockpitCard>
+</template>
+
+<script lang="ts" setup>
+import CockpitCard from '#components/cockpit/cockpitCard/CockpitCard.vue'
+
+type User = {
+  name: string
+  text?: string
+  place?: string
+  work?: string
+  social: {
+    facebook?: string
+    linkedin?: string
+    xing?: string
+    x?: string
+  }
+}
+
+// Dummy user. TODO replace by real data
+const user: User = {
+  name: 'John Doe',
+  text: 'Pushing pixels and experiences in digital products',
+  place: 'Berlin, Germany',
+  work: 'Frontend Developer at Company XY',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/johndoe',
+    xing: 'https://www.xing.com/profile/John_Doe',
+    x: 'https://www.x.com/johndoe',
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.details,
+.social {
+  padding: 0;
+  list-style: none;
+}
+
+.social {
+  display: flex;
+}
+
+hr {
+  margin-block: 10px;
+  border-color: white;
+}
+</style>
