57c881a6cf6e6ca986df2ca6a8909f672328aa37
Maximilian Harz <maxharz@gmail.com>
2024-07-03 19:00:28 +0200

    Add icons for about me section

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -1,11 +1,24 @@
 import Bell from './bell.svg?component'
 import Camera from './camera.svg?component'
 import Cockpit from './cockpit.svg?component'
+import Data from './data.svg?component'
+import Date from './date.svg?component'
 import Ellipsis from './ellipsis.svg?component'
+import Facebook from './facebook.svg?component'
+import Instagram from './instagram.svg?component'
+import LinkedIn from './linkedIn.svg?component'
 import Logout from './logout.svg?component'
 import Mall from './mall.svg?component'
 import Message from './message.svg?component'
+import Pinterest from './pinterest.svg?component'
+import Place from './place.svg?component'
+import Share from './share.svg?component'
+import Website from './website.svg?component'
+import Working from './working.svg?component'
 import WorldCafe from './worldCafe.svg?component'
+import X from './x.svg?component'
+import Xing from './xing.svg?component'
+import Youtube from './youtube.svg?component'
 
 import type { IconAliases } from 'vuetify'
 
@@ -13,11 +26,24 @@ const aliases: Partial<IconAliases> = {
   bell: Bell,
   camera: Camera,
   cockpit: Cockpit,
+  data: Data,
+  date: Date,
   ellipsis: Ellipsis,
+  facebook: Facebook,
+  instagram: Instagram,
+  linkedIn: LinkedIn,
   logout: Logout,
   mall: Mall,
   message: Message,
+  pinterest: Pinterest,
+  place: Place,
+  share: Share,
+  website: Website,
+  working: Working,
   'world-cafe': WorldCafe,
+  x: X,
+  xing: Xing,
+  youtube: Youtube,
 }
 
 export { aliases }
