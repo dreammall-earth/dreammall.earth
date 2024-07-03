c724f3ff15e331e519e23f07b81c25a22421b422
Maximilian Harz <maxharz@gmail.com>
2024-07-03 19:10:38 +0200

    Add cockpit page with about me

---
+++ frontend/src/pages/cockpit/+Page.vue
@@ -0,0 +1,10 @@
+<template>
+  <DefaultLayout>
+    <AboutMe />
+  </DefaultLayout>
+</template>
+
+<script lang="ts" setup>
+import AboutMe from '#components/cockpit/about-me/AboutMe.vue'
+import DefaultLayout from '#layouts/DefaultLayout.vue'
+</script>
