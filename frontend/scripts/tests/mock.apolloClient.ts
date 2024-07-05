58af99dac02bb976d7de0aa2831ddad9b736e880
Hannes Heine <heine.hannes@gmail.com>
2024-07-05 13:35:07 +0200

    feat(frontend): show open rooms sidebar (#1273)
    remove timeout for refetch rooms
    silent refresh without default layout
    no default lazyut on auth page
    more logs, use setRooms to update state
    delay refetch open rooms 1 min, more console to debug
    error handling refetch open rooms
    handle click event on open room
    type startTime in Room/OpenRoom is string
    quick and dirty fixing of List Drawer, open rooms query, rooms store
    Add rooms store.
    Add function of ListNavigationDrawer.
    Test remove handleOpenRooms.
    remove space
    change classname
    resolve conflicts, remove Navigatin Drawer Component
    remove unused prop
    add locale, change tsconfig
    coverage to 96%
    render config
    add toggle on camera icon, open sibebar
    Add SearchField Component
     change files and folders for atomic design
    add ListWithNavigationDrawer Component, change files and folders for atomic design
    NavigationDrawer Component
    Add ListElement Component
    * Removing console statement and fix linting, so that the CI runs through.
    
    Co-authored-by: Moriz Wahl <moriz.wahl@gmx.de>
    Co-authored-by: ogerly <ogerly@tutanota.com>

--- frontend/scripts/tests/mock.apolloClient.ts
+++ frontend/scripts/tests/mock.apolloClient.ts
@@ -1,8 +1,18 @@
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { config } from '@vue/test-utils'
 import { createMockClient } from 'mock-apollo-client'
+import { vi } from 'vitest'
+
+import { openRoomsQuery } from '#src/graphql/queries/openRoomsQuery'
 
 export const mockClient = createMockClient()
+
+export const openRoomsQueryMock = vi.fn()
+
+mockClient.setRequestHandler(
+  openRoomsQuery,
+  openRoomsQueryMock.mockResolvedValue({ data: { openRooms: [] } }),
+)
 
 config.global.provide = {
   ...config.global.provide,
--- frontend/src/components/menu/BottomMenu.vue
+++ frontend/src/components/menu/BottomMenu.vue
@@ -1,8 +1,15 @@
 <template>
+  <div class="navigation-drawer-box d-md-none d-lg-none position-fixed mb-5 pb-5">
+    <ListWithNavigationDrawer
+      :drawer="drawer"
+      :location="location"
+      @update:drawer="updateDrawer($event)"
+    />
+  </div>
   <div
-    class="bottom-menu d-flex w-100 position-fixed bottom-0 justify-space-around align-center py-2 bg-surface"
+    class="bottom-menu d-flex w-100 position-fixed bottom-0 justify-space-around align-center py-2 bg-surface d-md-none d-lg-none"
   >
-    <Circle class="camera-button">
+    <Circle class="camera-button" @click="toggleDrawer">
       <v-icon icon="$camera"></v-icon>
     </Circle>
     <CreateButtonMobile />
@@ -11,10 +18,22 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 import CreateButtonMobile from '#components/buttons/CreateButtonMobile.vue'
+import ListWithNavigationDrawer from '#components/vuetify/Organisms/ListWithNavigationDrawer.vue'
 
 import Circle from './CircleElement.vue'
 import UserInfo from './UserInfo.vue'
+
+const drawer = ref(false)
+const toggleDrawer = () => {
+  drawer.value = !drawer.value
+}
+const updateDrawer = (value: boolean) => {
+  drawer.value = value
+}
+const location = ref<'bottom' | 'right' | 'left' | 'end' | 'top' | 'start'>('bottom')
 </script>
 
 <style scoped lang="scss">
@@ -29,5 +48,22 @@ import UserInfo from './UserInfo.vue'
   .camera-button {
     transform: translateX(20px);
   }
+}
+
+.create-button-mobile {
+  z-index: 1;
+  transform: translate(20px, 30px);
+}
+
+.navigation-drawer-box {
+  bottom: 65px;
+}
+
+.v-navigation-drawer {
+  scrollbar-width: thin;
+}
+
+.v-navigation-drawer__content {
+  scrollbar-width: thin;
 }
 </style>
