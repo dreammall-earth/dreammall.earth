6024fd9803c4bde1b87afc353be194ba8c643ed7
Moriz Wahl <moriz.wahl@gmx.de>
2024-07-08 18:12:06 +0200

    remove depricated call of check for open rooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,6 @@
 // eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata'
 
-import { CONFIG } from '#config/config'
-import { checkForOpenRooms } from '#graphql/resolvers/dal/handleOpenRooms'
-
 import logger from './logger'
 import { prisma } from './prisma'
 import { listen } from './server/server'
@@ -11,8 +8,6 @@ import { listen } from './server/server'
 export const main = async (): Promise<void> => {
   await listen(4000)
   logger.info(`🚀 Server is ready at http://localhost:4000/`)
-  // dirty hack do avoid e2e Tests pulling on BBB
-  if (CONFIG.BBB_URL !== 'https://my.url') checkForOpenRooms()
 }
 
 void main()
