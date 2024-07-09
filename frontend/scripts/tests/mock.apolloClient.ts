508a1110be34e6d871a3243047f43ef6d32cb9bb
Moriz Wahl <moriz.wahl@gmx.de>
2024-07-09 14:24:16 +0200

    feat(backend): update open rooms via subscription (#1363)
    * remove depricated call of check for open rooms
    
    * update open rooms subscription with true data
    
    * add path to subscriptions, add update open rooms subscription
    
    * remove timeout as it will be a subscription
    
    * add update roooms subscription
    
    * remove subscription from index page and test the subscription in the store

--- frontend/scripts/tests/mock.apolloClient.ts
+++ frontend/scripts/tests/mock.apolloClient.ts
@@ -1,18 +1,23 @@
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { config } from '@vue/test-utils'
-import { createMockClient } from 'mock-apollo-client'
+import { createMockClient, createMockSubscription, IMockSubscription } from 'mock-apollo-client'
 import { vi } from 'vitest'
 
 import { openRoomsQuery } from '#src/graphql/queries/openRoomsQuery'
+import { updateOpenRoomsSubscription } from '#subscriptions/updateOpenRoomsSubscription'
 
 export const mockClient = createMockClient()
 
 export const openRoomsQueryMock = vi.fn()
 
+export const updateOpenRoomsSubscriptionMock: IMockSubscription = createMockSubscription()
+
 mockClient.setRequestHandler(
   openRoomsQuery,
   openRoomsQueryMock.mockResolvedValue({ data: { openRooms: [] } }),
 )
+
+mockClient.setRequestHandler(updateOpenRoomsSubscription, () => updateOpenRoomsSubscriptionMock)
 
 config.global.provide = {
   ...config.global.provide,
