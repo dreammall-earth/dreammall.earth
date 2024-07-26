ae83f7a658c10f41407e6c6bba7b269f3f9dace5
Maximilian Harz <maxharz@gmail.com>
2024-07-26 12:24:18 +0200

    Move TopMenu and BottomMenu to DefaultLayout

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,30 +1,82 @@
 <template>
   <v-main class="bg-background main-layout">
-    <TopMenu />
-    <v-container fluid class="page-container px-8">
-      <v-row>
-        <v-col v-if="slots.sidemenu" cols="2">
-          <v-sheet rounded>
-            <slot name="sidemenu"></slot>
-          </v-sheet>
+    <v-app-bar flat class="app-bar" height="70">
+      <v-row class="ma-1">
+        <v-col class="d-none d-md-flex align-center">
+          <a href="/" class="logo">
+            <LogoImage size="small" />
+          </a>
         </v-col>
-
-        <v-col class="pa-0 pa-sm-1 pt-0">
-          <slot />
+        <v-col class="d-flex align-center justify-center">
+          <TabControl />
+        </v-col>
+        <v-col class="d-none d-md-flex align-center">
+          <v-row>
+            <v-col class="d-flex align-center">
+              <LightDarkSwitch class="d-none d-lg-flex" />
+            </v-col>
+            <v-col class="d-flex align-center justify-end">
+              <button @click="toggleDrawer">
+                <Circle>
+                  <v-icon icon="$camera"></v-icon>
+                </Circle>
+              </button>
+              <UserInfo class="ml-2" />
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
-    </v-container>
-    <BottomMenu />
+    </v-app-bar>
+    <TablesDrawer v-model="isTablesDrawerVisible" class="hide-on-mobile" location="right" />
+    <div class="page-container px-8">
+      <slot />
+    </div>
+    <MobileCreateButtonActions :is-visible="isButtonListVisible" />
+    <TablesDrawer v-model="isTablesDrawerVisible" location="bottom" />
+    <div class="bottom-menu w-100 position-fixed bottom-0 py-2 d-md-none">
+      <button class="camera-button mx-auto" @click="toggleDrawer">
+        <Circle>
+          <v-icon icon="$camera"></v-icon>
+        </Circle>
+      </button>
+      <MobileCreateButton
+        class="mx-auto"
+        :is-active="isButtonListVisible"
+        @button-click="toggleButtonList"
+      />
+      <UserInfo class="mx-auto" />
+    </div>
   </v-main>
 </template>
 
 <script lang="ts" setup>
-import { useSlots } from 'vue'
+import { ref } from 'vue'
 
-import BottomMenu from '#components/menu/BottomMenu.vue'
-import TopMenu from '#components/menu/TopMenu.vue'
+import MobileCreateButton from '#components/buttons/mobile-create-button/MobileCreateButton.vue'
+import MobileCreateButtonActions from '#components/buttons/mobile-create-button/MobileCreateButtonActions.vue'
+import Circle from '#components/menu/CircleElement.vue'
+import LightDarkSwitch from '#components/menu/LightDarkSwitch.vue'
+import LogoImage from '#components/menu/LogoImage.vue'
+import TabControl from '#components/menu/TabControl.vue'
+import UserInfo from '#components/menu/UserInfo.vue'
+import TablesDrawer from '#components/tablesDrawer/TablesDrawer.vue'
 
-const slots = useSlots()
+const isTablesDrawerVisible = defineModel<boolean>()
+
+const toggleDrawer = () => {
+  isTablesDrawerVisible.value = !isTablesDrawerVisible.value
+}
+
+const isButtonListVisible = ref(false)
+
+const toggleButtonList = () => {
+  try {
+    isButtonListVisible.value = !isButtonListVisible.value
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error while MobileCreateButton click', error)
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -45,5 +97,39 @@ const slots = useSlots()
       margin-bottom: 50px;
     }
   }
+}
+
+.app-bar {
+  background: transparent !important;
+}
+
+.logo {
+  width: 140px;
+}
+
+.hide-on-mobile {
+  @media #{map-get($display-breakpoints, 'md-and-down')} {
+    display: none;
+  }
+}
+
+.bottom-menu {
+  bottom: 0;
+  left: 0;
+  z-index: 3000;
+  display: grid;
+  grid-template-columns: repeat(3, 33.3333%);
+  background: var(--v-bottom-menu-background);
+  backdrop-filter: blur(20px);
+  border-radius: 30px 30px 0 0;
+
+  .camera-button {
+    transform: translateX(20px);
+  }
+}
+
+.create-button-mobile {
+  z-index: 1;
+  transform: translate(20px, 30px);
 }
 </style>
