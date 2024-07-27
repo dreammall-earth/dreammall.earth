b8fa8f945e12209df03e8c119d8ff0b72badbd85
mahula <lenzmath@posteo.de>
2024-07-27 10:34:14 +0200

    remove accidentally copied files

--- frontend/src/pages/join-table/join-room/+guard.ts
+++
@@ -1,6 +0,0 @@
-import type { GuardAsync } from 'vike/types'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const guard: GuardAsync = async (pageContext): ReturnType<GuardAsync> => {}
-
-export { guard }
