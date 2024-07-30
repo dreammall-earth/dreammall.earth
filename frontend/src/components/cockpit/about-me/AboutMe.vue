14d4ff68b9bc4afc8c1a34c4405aa35a4db7ff1f
Maximilian Harz <maxharz@gmail.com>
2024-07-30 19:54:53 +0200

    Change about me propierties

--- frontend/src/components/cockpit/about-me/AboutMe.vue
+++ frontend/src/components/cockpit/about-me/AboutMe.vue
@@ -5,30 +5,30 @@
     </template>
     <template #default>
       <!-- eslint-disable-next-line @intlify/vue-i18n/no-raw-text -->
-      <div class="about-me-text">"{{ user.text }}"</div>
+      <div class="introduction">"{{ props.introduction }}"</div>
       <hr />
       <ul class="details">
-        <li v-if="user.place"><v-icon icon="$place"></v-icon> {{ user.place }}</li>
-        <li v-if="user.work"><v-icon icon="$working"></v-icon> {{ user.work }}</li>
-        <li></li>
+        <v-for :detail="props.details">
+          <li><v-icon :icon="getIcon(detail.category)"></v-icon> {{ detail.text }}</li>
+        </v-for>
       </ul>
       <ul class="social">
-        <li v-if="user.social.facebook">
+        <li v-if="props.social.facebook">
           <a :href="user.social.facebook" target="_blank" rel="noopener noreferrer">
             <v-icon icon="$facebook"></v-icon>
           </a>
         </li>
-        <li v-if="user.social.linkedin">
+        <li v-if="props.social.linkedin">
           <a :href="user.social.linkedin" target="_blank" rel="noopener noreferrer">
             <v-icon icon="$linkedIn"></v-icon>
           </a>
         </li>
-        <li v-if="user.social.xing">
+        <li v-if="props.social.xing">
           <a :href="user.social.xing" target="_blank" rel="noopener noreferrer">
             <v-icon icon="$xing"></v-icon>
           </a>
         </li>
-        <li v-if="user.social.x">
+        <li v-if="props.social.x">
           <a :href="user.social.x" target="_blank" rel="noopener noreferrer">
             <v-icon icon="$x"></v-icon>
           </a>
@@ -41,26 +41,56 @@
 <script lang="ts" setup>
 import CockpitCard from '#components/cockpit/cockpitCard/CockpitCard.vue'
 
-type User = {
+type DetailCategory = 'place' | 'work' | 'language' | 'education' | 'feeling'
+
+type Detail = {
+  category: DetailCategory
+  text: string
+}
+
+const props = defineProps<{
   name: string
-  text?: string
-  place?: string
-  work?: string
+  introduction?: string
+  availability?: 'available' | 'partly_available' | 'busy'
+  details: Detail[]
   social: {
+    discord?: string
+    telegram?: string
     facebook?: string
+    tiktok?: string
+    snapchat?: string
+    reddit?: string
+    wechat?: string
+    instagram?: string
+    pintarest?: string
     linkedin?: string
+    youtube?: string
+    whatsapp?: string
     xing?: string
     x?: string
   }
-}
-
-defineProps<{
-  user: User
 }>()
+
+function getIcon(category: DetailCategory) {
+  switch (category) {
+    case 'place':
+      return '$location'
+    case 'work':
+      return '$work'
+    case 'language':
+      return '$language'
+    case 'education':
+      return '$education'
+    case 'feeling':
+      return '$feeling'
+    default:
+      throw new Error(`Unknown category: ${category}`)
+  }
+}
 </script>
 
 <style scoped lang="scss">
-.about-me-text {
+.introduction {
   overflow-wrap: break-word;
 }
 
