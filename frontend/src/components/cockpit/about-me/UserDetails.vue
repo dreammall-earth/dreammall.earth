921e183670a61c77e792726759aa2b36ec5d4d29
Maximilian Harz <maxharz@gmail.com>
2024-08-08 16:50:59 +0200

    Show placeholders for detail categories without values

--- frontend/src/components/cockpit/about-me/UserDetails.vue
+++ frontend/src/components/cockpit/about-me/UserDetails.vue
@@ -1,10 +1,14 @@
 <template>
   <ul class="details">
-    <li v-for="(detail, index) in props.details" :key="index">
+    <li v-for="(detail, index) in details" :key="index">
       <v-chip class="detail">
         <v-icon :icon="detailCategoryToIcon(detail.category)" class="mr-2"></v-icon>
         {{ detail.text }}
-        <v-icon v-if="props.editable" icon="$close" @click="$emit('remove-detail', detail.id)">
+        <v-icon
+          v-if="props.editable && detail.id >= 0"
+          icon="$close"
+          @click="$emit('remove-detail', detail.id)"
+        >
         </v-icon
       ></v-chip>
     </li>
@@ -12,9 +16,15 @@
 </template>
 
 <script setup lang="ts">
+import { computed, Ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { detailCategories } from './detailCategories'
 import { detailCategoryToIcon } from './detailCategoryToIcon'
 
 import type { UserDetail } from '#stores/userStore'
+
+const { t } = useI18n()
 
 const props = defineProps<{
   details: UserDetail[]
@@ -24,6 +34,14 @@ const props = defineProps<{
 defineEmits<{
   (e: 'remove-detail', id: number): void
 }>()
+
+const details: Ref<UserDetail[]> = computed(() =>
+  detailCategories.map((category) => {
+    const detail = props.details.find((detail) => detail.category === category)
+    if (detail) return detail
+    return { id: -1, category, text: t(`cockpit.about-me.${category}-placeholder`) }
+  }),
+)
 </script>
 
 <style scoped>
