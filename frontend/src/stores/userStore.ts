e05a5751de43c6e9d26245dc242413fc7fa0c0fe
Max <maxharz@gmail.com>
2024-08-13 23:02:44 +0200

    feat(frontend): add mutations for about me, update user store and user query (#1689)
    - Adds mutations for adding and removing user details and social media accounts
    - Adds a mutation to update availability and name of a user
    - Modifies the user query to contain these informations
    - Updates the user store, which now uses those mutations and exposes them as actions
    
    Will be used by #1327.
    
    ### Issues
    - fixes part of #1018

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -1,8 +1,28 @@
-import { useQuery } from '@vue/apollo-composable'
+import { useMutation, useQuery } from '@vue/apollo-composable'
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { computed, ref, watch } from 'vue'
 
+import {
+  addSocialMediaMutation,
+  AddSocialMediaInput,
+  AddSocialMediaMutationResult,
+} from '#mutations/addSocialMediaMutation'
+import {
+  addUserDetailMutation,
+  AddUserDetailInput,
+  AddUserDetailMutationResult,
+} from '#mutations/addUserDetailMutation'
+import { removeSocialMediaMutation } from '#mutations/removeSocialMediaMutation'
+import { removeUserDetailMutation } from '#mutations/removeUserDetailMutation'
+import {
+  updateUserMutation,
+  UpdateUserInput,
+  UpdateUserMutationResult,
+} from '#mutations/updateUserMutation'
 import { currentUserQuery } from '#queries/currentUserQuery'
+
+export type { AddSocialMediaInput } from '#mutations/addSocialMediaMutation'
+export type { AddUserDetailInput } from '#mutations/addUserDetailMutation'
 
 export type UserInTable = {
   id: number
@@ -18,12 +38,32 @@ export type MyTable = {
   users: UserInTable[]
 }
 
+export type UserDetailCategory = 'place' | 'work' | 'language' | 'education' | 'feeling'
+
+export type UserDetail = {
+  id: number
+  category: UserDetailCategory
+  text: string
+}
+
+export type UserAvailability = null | 'available' | 'partly_available' | 'busy'
+
+export type SocialMedia = {
+  id: number
+  type: string
+  link: string
+}
+
 export type CurrentUser = {
   id: number
   name: string
   username: string
   avatar?: string
   table?: MyTable
+  introduction?: string
+  availability: UserAvailability
+  details: UserDetail[]
+  social: SocialMedia[]
 }
 
 export const useUserStore = defineStore(
@@ -31,7 +71,7 @@ export const useUserStore = defineStore(
   () => {
     const currentUser = ref<CurrentUser | null>(null)
 
-    const { result: currentUserQueryResult } = useQuery(
+    const { result: currentUserQueryResult, loading } = useQuery(
       currentUserQuery,
       {},
       {
@@ -66,7 +106,84 @@ export const useUserStore = defineStore(
       currentUser.value = user
     }
 
+    const { mutate: updateUserMutationResult } = useMutation<UpdateUserMutationResult>(
+      updateUserMutation,
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
+
+    const updateUser = async (data: UpdateUserInput) => {
+      const result = await updateUserMutationResult({ data })
+      if (result?.data) {
+        setCurrentUser(result.data.updateUser)
+      }
+    }
+
+    const { mutate: addUserDetailMutationResult } = useMutation<AddUserDetailMutationResult>(
+      addUserDetailMutation,
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
+
+    const addUserDetail = async (userDetail: AddUserDetailInput) => {
+      const result = await addUserDetailMutationResult({ data: userDetail })
+      if (result?.data && currentUser.value?.id) {
+        setCurrentUser({
+          ...currentUser.value,
+          details: [...currentUser.value.details, result.data.addUserDetail],
+        })
+      }
+    }
+
+    const { mutate: removeUserDetailMutationResult } = useMutation(removeUserDetailMutation, {
+      fetchPolicy: 'no-cache',
+    })
+
+    const removeUserDetail = async (userDetailId: number) => {
+      await removeUserDetailMutationResult({ id: userDetailId })
+      if (currentUser.value?.id) {
+        setCurrentUser({
+          ...currentUser.value,
+          details: currentUser.value.details.filter((d) => d.id !== userDetailId),
+        })
+      }
+    }
+
+    const { mutate: AddSocialMediaMutationResult } = useMutation<AddSocialMediaMutationResult>(
+      addSocialMediaMutation,
+      {
+        fetchPolicy: 'no-cache',
+      },
+    )
+
+    const addSocialMedia = async (socialMedia: AddSocialMediaInput) => {
+      const result = await AddSocialMediaMutationResult({ data: socialMedia })
+      if (result?.data && currentUser.value?.id) {
+        setCurrentUser({
+          ...currentUser.value,
+          social: [...currentUser.value.social, result.data.addSocialMedia],
+        })
+      }
+    }
+
+    const { mutate: removeSocialMediaMutationResult } = useMutation(removeSocialMediaMutation, {
+      fetchPolicy: 'no-cache',
+    })
+
+    const removeSocialMedia = async (socialMediaId: number) => {
+      await removeSocialMediaMutationResult({ id: socialMediaId })
+      if (currentUser.value?.id) {
+        setCurrentUser({
+          ...currentUser.value,
+          social: currentUser.value.social.filter((s) => s.id !== socialMediaId),
+        })
+      }
+    }
+
     return {
+      loading,
       currentUser,
       getCurrentUser,
       setCurrentUser,
@@ -74,10 +191,15 @@ export const useUserStore = defineStore(
       getCurrentUserAvatar,
       getMyTable,
       getUsersInMyTable,
+      updateUser,
+      addUserDetail,
+      addSocialMedia,
+      removeUserDetail,
+      removeSocialMedia,
     }
   },
   {
-    persist: true,
+    persist: false,
   },
 )
 
