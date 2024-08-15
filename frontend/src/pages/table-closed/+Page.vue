fcfcd8f3a3893e104bf761165cd0868a21345474
Moriz Wahl <moriz.wahl@gmx.de>
2024-08-15 17:08:00 +0200

    add table-closed page

---
+++ frontend/src/pages/table-closed/+Page.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { navigate } from 'vike/client/router'
+import { onBeforeMount } from 'vue'
+
+onBeforeMount(() => {
+  navigate('/')
+})
+</script>
