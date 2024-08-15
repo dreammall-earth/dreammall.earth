91128d3ec29774db3a6673fd43dbc8e4015ee80d
Maximilian Harz <maxharz@gmail.com>
2024-08-15 20:37:13 +0200

    Trigger navigate to home by postMessage, remove iframe border

--- frontend/src/pages/table-closed/+Page.vue
+++ frontend/src/pages/table-closed/+Page.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script setup lang="ts">
-import { navigate } from 'vike/client/router'
-import { onBeforeMount } from 'vue'
+import { onMounted } from 'vue'
 
-onBeforeMount(() => {
-  navigate('/')
+onMounted(() => {
+  window.parent.postMessage('close', '*')
 })
 </script>
