f1e1c59aabd5d76360195f14f07ae9a95601c161
Robert Schäfer <git@roschaefer.de>
2024-08-15 18:36:03 +0200

    refactor(admin): remove admin application
    Motivation
    ----------
    If we implement role-based authorization, we will enforce it in the
    backend and we will teach the frontend how to show a separate interface
    to users with a role `admin`.
    
    This PR will improve the following:
    
    * Less dependencies + dependabot PRs
    * Less workflows to run
    * Less code duplication
    * Less memory consumption in our kubernetes cluster
    * Less domains to generate certificates for
    * Less potential for CORS issues
    * Less containers running in `docker-compose`
    * Less disk usage on our laptops
    
    How to test
    -----------
    1. After this gets merged, we should test our vServer deployment
    
    close #1785

--- infrastructure/vserver/bootstrap.sh
+++ infrastructure/vserver/bootstrap.sh
@@ -24,19 +24,16 @@ PROJECT_ROOT=/var/www/localhost/htdocs/dreammall.earth
 mkdir -p /etc/nginx/http.d
 cp -f $PROJECT_ROOT/deployment/nginx/default.conf /etc/nginx/http.d/default.conf
 cp $PROJECT_ROOT/deployment/nginx/frontend.conf /etc/nginx/http.d/frontend.conf
-cp $PROJECT_ROOT/deployment/nginx/admin.conf /etc/nginx/http.d/admin.conf
 
 mysql -e "CREATE USER 'dreammall'@'localhost' IDENTIFIED BY 'SECRET'; GRANT ALL PRIVILEGES ON * . * TO 'dreammall'@'localhost'; FLUSH PRIVILEGES;"
 
 cp $PROJECT_ROOT/backend/.env.dist $PROJECT_ROOT/backend/.env
 cp $PROJECT_ROOT/presenter/.env.dist $PROJECT_ROOT/presenter/.env
 cp $PROJECT_ROOT/frontend/.env.dist $PROJECT_ROOT/frontend/.env
-cp $PROJECT_ROOT/admin/.env.dist $PROJECT_ROOT/admin/.env
 
 # The target ports here must match the port mappings of the Vagrant file
 sed -e 's/localhost:3001/localhost:8000/g' -i $PROJECT_ROOT/*/.env
 sed -e 's/localhost:4000/localhost:8000\/api/g' -i $PROJECT_ROOT/*/.env
 sed -e 's/localhost:3000/localhost:8080/g' -i $PROJECT_ROOT/*/.env
-sed -e 's/localhost:3002/localhost:8082/g' -i $PROJECT_ROOT/*/.env
 
 $PROJECT_ROOT/deployment/deploy.sh
