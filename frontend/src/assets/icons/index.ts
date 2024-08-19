6dc7e5a89afd1a8959155551fd0450b66a3970af
Maximilian Harz <maxharz@gmail.com>
2024-08-19 22:30:21 +0200

    Remove unused icons

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -1,57 +1,35 @@
-import Back from './back.svg?component'
 import Bell from './bell.svg?component'
 import Camera from './camera.svg?component'
 import Cockpit from './cockpit.svg?component'
-import Data from './data.svg?component'
-import Date from './date.svg?component'
 import Discord from './discord.svg?component'
 import Ellipsis from './ellipsis.svg?component'
-import Facebook from './facebook.svg?component'
-import Instagram from './instagram.svg?component'
-import LinkedIn from './linkedIn.svg?component'
 import Logout from './logout.svg?component'
 import Mall from './mall.svg?component'
 import Message from './message.svg?component'
-import Pinterest from './pinterest.svg?component'
-import Place from './place.svg?component'
-import Share from './share.svg?component'
 import Telegram from './telegram.svg?component'
 import Tiktok from './tiktok.svg?component'
-import Triangle from './triangle.svg?component'
 import Website from './website.svg?component'
 import WorldCafe from './worldCafe.svg?component'
 import X from './x.svg?component'
 import Xing from './xing.svg?component'
-import Youtube from './youtube.svg?component'
 
 import type { IconAliases } from 'vuetify'
 
 const aliases: Partial<IconAliases> = {
-  back: Back,
   bell: Bell,
   camera: Camera,
   cockpit: Cockpit,
-  data: Data,
-  date: Date,
   discord: Discord,
   ellipsis: Ellipsis,
-  facebook: Facebook,
-  instagram: Instagram,
-  linkedIn: LinkedIn,
   logout: Logout,
   mall: Mall,
   message: Message,
-  pinterest: Pinterest,
-  place: Place,
-  share: Share,
   telegram: Telegram,
   tiktok: Tiktok,
-  triange: Triangle,
   website: Website,
   'world-cafe': WorldCafe,
   x: X,
   xing: Xing,
-  youtube: Youtube,
 }
 
 export { aliases }
