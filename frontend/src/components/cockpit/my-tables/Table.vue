0f38220dc6b37177a036ffc51efaeec96ba381a1
Maximilian Harz <maxharz@gmail.com>
2024-08-21 21:31:00 +0200

    Add animated option list

--- frontend/src/components/cockpit/my-tables/Table.vue
+++ frontend/src/components/cockpit/my-tables/Table.vue
@@ -7,11 +7,22 @@
       </span>
     </div>
     <button class="action" @click="openTable">
-      <v-icon class="icon" icon="$camera" />
+      <v-icon class="camera-icon" icon="$camera" />
     </button>
     <button class="options" @click="toggleOptions">
-      <v-icon class="icon" icon="mdi mdi-ellipsis" />
+      <v-icon
+        class="options-icon"
+        :class="{ turned: isShowingOptions }"
+        icon="mdi mdi-dots-horizontal"
+      />
     </button>
+    <TransitionGroup name="options">
+      <ul v-if="isShowingOptions" class="options-list">
+        <li>
+          <button>Edit</button>
+        </li>
+      </ul>
+    </TransitionGroup>
   </li>
 </template>
 
@@ -32,16 +43,17 @@ const openTable = () => {
 }
 
 const toggleOptions = () => {
-  isShowingOptions.value = true
+  isShowingOptions.value = !isShowingOptions.value
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .table {
   display: flex;
   gap: 8px;
   align-items: center;
   justify-content: center;
+  position: relative;
 }
 
 .table-info {
@@ -76,7 +88,49 @@ const toggleOptions = () => {
   border-radius: 0 16px 16px 0;
 }
 
-.icon {
+.camera-icon {
   transform: scale(0.8);
+}
+
+.options-icon {
+  transform: rotate(0);
+  transition: transform 0.3s;
+  &.turned {
+    transform: rotate(90deg);
+  }
+}
+
+.options-list {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: calc(100% - 30px);
+  display: flex;
+  flex-flow: column;
+  gap: 8px;
+  padding: 8px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  list-style: none;
+}
+
+.options-move, /* apply transition to moving elements */
+.options-enter-active,
+.options-leave-active {
+  transition: all 0.3s ease;
+}
+
+.options-enter-from,
+.options-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.options-leave-active {
+  position: absolute;
 }
 </style>
