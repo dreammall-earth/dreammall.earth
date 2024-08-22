6ebf34c6f2cbb6a21a83bb6d8c005f4be5c562ee
Max <maxharz@gmail.com>
2024-08-22 09:30:28 +0200

    fix(backend): derive INVITE_LINK_URL and BBB_LOGOUT_URL from FRONTEND_URL (#1820)
    In our kubernetes Deployment, the environment variable FRONTEND_BBB_LOGOUT_URL was missing. Therefore the fix for #1261 did not work.
    
    Instead of adding the environment variable, I propose to simplify the config and derive its value, as well as the one for invite links, from the FRONTEND_URL.

--- backend/src/graphql/resolvers/TableResolver.spec.ts
+++ backend/src/graphql/resolvers/TableResolver.spec.ts
@@ -18,7 +18,7 @@ const getMeetingsMock = jest.mocked(getMeetings)
 
 let testServer: ApolloServer<Context>
 
-CONFIG.FRONTEND_INVITE_LINK_URL = '/'
+CONFIG.FRONTEND_URL = 'https://my.frontend.url'
 
 const nickname = 'mockedUser'
 const name = 'User'
@@ -808,7 +808,7 @@ describe('TableResolver', () => {
               meetingID: result?.meeting?.meetingID,
             },
             {
-              moderatorOnlyMessage: `Use this link to invite more people:<br/>/${result?.meeting?.id}`,
+              moderatorOnlyMessage: `Use this link to invite more people:<br/>https://my.frontend.url/join-table/${result?.meeting?.id}`,
             },
           )
         })
