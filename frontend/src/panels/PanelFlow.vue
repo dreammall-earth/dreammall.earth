f784337c9cc70cdb481f033f17469ef4a754eb3a
Sebastian Stein <git@codepassion.de>
2024-08-29 09:53:42 +0200

    - prepared refactoring for PanelFlow

--- frontend/src/panels/PanelFlow.vue
+++ frontend/src/panels/PanelFlow.vue
@@ -1,101 +1,125 @@
+<!--&lt;!&ndash;Prepared for refactoring panel flow. Not Ready to use &ndash;&gt;-->
 <!--<template>-->
-<!--  <v-card>-->
-<!--    <v-card-title>-->
-<!--      <v-btn v-if="currentStep > 0" icon @click="goBack">-->
-<!--        <v-icon>mdi-arrow-left</v-icon>-->
-<!--      </v-btn>-->
-<!--      {{ title }}-->
-<!--    </v-card-title>-->
-<!--    <v-card-text>-->
-<!--      <component-->
-<!--        :is="steps[currentStep].stepName"-->
-<!--        v-bind="componentProps"-->
-<!--        @next="goNext"-->
-<!--      ></component>-->
-<!--    </v-card-text>-->
-<!--  </v-card>-->
+<!--  <PanelHeader-->
+<!--    v-if="steps"-->
+<!--    :title="steps[currentStep]?.title ?? 'unknown'"-->
+<!--    :is-back-button-visible="currentStep > 0"-->
+<!--    :is-close-button-visible="true"-->
+<!--    @back="onBack"-->
+<!--    @close="onClose"-->
+<!--  />-->
+<!--  <component-->
+<!--    :is="steps[currentStep].component"-->
+<!--    v-if="steps && currentStep < steps.length"-->
+<!--    v-bind="componentProps"-->
+<!--    v-on="componentEmits"-->
+<!--  />-->
 <!--</template>-->
 
-<!--<script lang="ts">-->
-<!--import { ref, computed, onMounted, onUnmounted, PropType, defineComponent } from 'vue'-->
+<!--<script setup lang="ts" generic="T extends PanelFlowProps">-->
+<!--import { ref, onMounted, onUnmounted } from 'vue'-->
 
-<!--// Interface that all step components should implement-->
-<!--export interface StepComponent {-->
-<!--  stepName: string-->
-<!--  validate?: () => boolean | Promise<boolean>-->
+<!--import PanelHeader from '#src/panels/PanelHeader.vue'-->
+
+<!--type StepId = string | (() => string)-->
+<!--export type Step = {-->
+<!--  component: unknown-->
+<!--  id: string-->
+<!--  title: string-->
+<!--  submit: StepId-->
+<!--  back: StepId-->
 <!--}-->
 
-<!--export default defineComponent({-->
-<!--  name: 'FlowComponent',-->
-<!--  props: {-->
-<!--    steps: {-->
-<!--      type: Array as PropType<StepComponent[]>,-->
-<!--      required: true,-->
-<!--      validator: (steps: StepComponent[]) => steps.every((step) => step.validate),-->
-<!--    },-->
-<!--    title: {-->
-<!--      type: String,-->
-<!--      default: 'Flow',-->
-<!--    },-->
-<!--    id: {-->
-<!--      type: String,-->
-<!--      required: true,-->
-<!--    },-->
+<!--export type PanelFlowProps = {-->
+<!--  steps: Step[]-->
+<!--}-->
+
+<!--export type PanelFlowEmits<T> = {-->
+<!--  (e: 'close'): void-->
+<!--  (e: 'complete'): void-->
+<!--  (e: 'update:componentProps', value: T): void-->
+<!--}-->
+
+<!--const props = defineProps<T>()-->
+<!--const emit = defineEmits<PanelFlowEmits<T>>()-->
+<!--const componentProps = ref<T>()-->
+
+<!--const currentStep = ref(0)-->
+
+<!--const transitToNext = () => transitToId(props.steps[currentStep.value].submit)-->
+<!--const transitToPrevious = () => transitToId(props.steps[currentStep.value].back)-->
+
+<!--const transitToId = (destinationId: StepId) => {-->
+<!--  if (destinationId === 'next') {-->
+<!--    if (currentStep.value < props.steps.length - 1) {-->
+<!--      currentStep.value++-->
+<!--    } else {-->
+<!--      emit('complete')-->
+<!--    }-->
+<!--  } else if (destinationId === 'previous') {-->
+<!--    if (currentStep.value > 0) {-->
+<!--      currentStep.value&#45;&#45;-->
+<!--    } else {-->
+<!--      emit('close')-->
+<!--    }-->
+<!--  } else if (typeof destinationId === 'string') {-->
+<!--    currentStep.value = findStepById(destinationId)-->
+<!--  } else if (typeof destinationId === 'function') {-->
+<!--    currentStep.value = findStepById(destinationId())-->
+<!--  }-->
+
+<!--  if (currentStep.value >= 0) {-->
+<!--    updateHistory(currentStep.value)-->
+<!--  }-->
+<!--}-->
+
+<!--const findStepById = (id: string): number => props.steps.findIndex((step) => step.id === id)-->
+
+<!--const onNext = transitToNext-->
+<!--const onBack = transitToPrevious-->
+<!--const onClose = () => emit('close')-->
+
+<!--const componentEmits = {-->
+<!--  next: onNext,-->
+<!--  back: onBack,-->
+<!--  close: onClose,-->
+<!--  'update:componentProps': (value: T) => {-->
+<!--    componentProps.value = value-->
+<!--    return emit('update:componentProps', value)-->
 <!--  },-->
-<!--  setup(props) {-->
-<!--    const currentStep = ref(0)-->
-<!--    const flowData = ref({})-->
+<!--}-->
 
-<!--    const componentProps = computed(() => ({-->
-<!--      ...flowData.value,-->
-<!--      modelValue: flowData.value,-->
-<!--    }))-->
+<!--const reset = () => {-->
+<!--  currentStep.value = 0-->
+<!--  updateHistory(currentStep.value)-->
+<!--}-->
+<!--defineExpose({ reset })-->
 
-<!--    const goNext = () => {-->
-<!--      if (currentStep.value < props.steps.length - 1) {-->
-<!--        currentStep.value++-->
-<!--        updateHistory()-->
-<!--      }-->
-<!--    }-->
+<!--const handlePopState = (event: PopStateEvent) => {-->
+<!--  if (event.state && typeof event.state.step === 'number') {-->
+<!--    currentStep.value = event.state.step-->
+<!--  }-->
+<!--}-->
 
-<!--    const goBack = () => {-->
-<!--      if (currentStep.value > 0) {-->
-<!--        currentStep.value&#45;&#45;-->
-<!--        updateHistory()-->
-<!--      }-->
-<!--    }-->
+<!--const updateHistory = (step: number) => {-->
+<!--  // if (window?.location?.href == null) return-->
+<!--  // const url = new URL(window.location.href)-->
+<!--  // url.searchParams.set('step', step.toString())-->
+<!--  // todo: find other solution. This causes conflicts with Vike-->
+<!--  // window.history.pushState({ step }, '', url.toString())-->
+<!--}-->
 
-<!--    const updateHistory = () => {-->
-<!--      const state = {-->
-<!--        flowId: props.id,-->
-<!--        step: currentStep.value,-->
-<!--        data: JSON.parse(JSON.stringify(flowData.value)),-->
-<!--      }-->
-<!--      history.pushState(state, '', `#${props.id}-${currentStep.value}`)-->
-<!--    }-->
+<!--onMounted(() => {-->
+<!--  window.addEventListener('popstate', handlePopState)-->
 
-<!--    const handlePopState = (event) => {-->
-<!--      if (event.state && event.state.flowId === props.id) {-->
-<!--        currentStep.value = event.state.step-->
-<!--        flowData.value = event.state.data-->
-<!--      }-->
-<!--    }-->
+<!--  const urlParams = new URLSearchParams(window.location.search)-->
+<!--  const initialStep = parseInt(urlParams.get('step') || '0', 10)-->
+<!--  currentStep.value =-->
+<!--    isNaN(initialStep) || initialStep < 0 || initialStep >= props.steps.length ? 0 : initialStep-->
+<!--  updateHistory(currentStep.value)-->
+<!--})-->
 
-<!--    onMounted(() => {-->
-<!--      updateHistory() // Set initial history state-->
-<!--      window.addEventListener('popstate', handlePopState)-->
-<!--    })-->
-
-<!--    onUnmounted(() => {-->
-<!--      window.removeEventListener('popstate', handlePopState)-->
-<!--    })-->
-
-<!--    return {-->
-<!--      currentStep,-->
-<!--      componentProps,-->
-<!--      goNext,-->
-<!--      goBack,-->
-<!--    }-->
-<!--  },-->
+<!--onUnmounted(() => {-->
+<!--  window.removeEventListener('popstate', handlePopState)-->
 <!--})-->
 <!--</script>-->
