fbb213a8e6e7731e610a1acd22805917ced8513d
Moriz Wahl <moriz.wahl@gmx.de>
2024-09-06 12:05:44 +0200

    simple user seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,8 +1,39 @@
+// eslint-disable-next-line n/no-unpublished-import
+import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {}
+const seedUsers = async () => {
+  await prisma.user.createMany({
+    data: [
+      {
+        username: 'bibi',
+        name: 'Bibi Bloxberg',
+      },
+      {
+        username: 'peter',
+        name: 'Peter Lustig',
+      },
+      {
+        username: 'bob',
+        name: 'Bob der Baumeister',
+      },
+      {
+        username: 'raeuber',
+        name: 'Räuber Hotzenplotz',
+      },
+      ...Array.from(new Array(100), () => ({
+        name: faker.person.fullName(),
+        username: faker.internet.userName(),
+      })),
+    ],
+  })
+}
+
+async function main() {
+  await seedUsers()
+}
 
 main()
   .then(async () => {
