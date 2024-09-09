7c77691ecfa25ba8585bce8331165a8219573356
Sebastian Stein <git@codepassion.de>
2024-09-09 14:58:51 +0200

    - added new layout for actions - removed edit action - overtook colors from drafts - activated leave-button

--- frontend/src/components/malltalk/settings/TableSettingsRoot.vue
+++ frontend/src/components/malltalk/settings/TableSettingsRoot.vue
@@ -7,6 +7,10 @@
             <v-icon icon="mdi-link" class="mr-2" />
             {{ $t('dream-mall-panel.call.link') }}
           </v-btn>
+          <v-btn class="px-6" @click="onClick('settings')">
+            <v-icon icon="mdi-pencil" class="mr-2" />
+            {{ $t('dream-mall-panel.call.edit') }}
+          </v-btn>
           <v-btn class="px-6" @click="onClick('users')">
             <v-icon icon="mdi-account-plus" class="mr-2" />
             {{ $t('dream-mall-panel.call.add-user') }}
@@ -15,15 +19,17 @@
       </v-col>
     </v-row>
     <SimpleButton
-      color="#F44336"
       :label="$t('dream-mall-panel.call.leave-table')"
-      class="mt-12 mb-5"
-      disabled="disabled"
+      class="leaveButton mt-12 mb-5"
+      @click="leaveTable"
     />
   </div>
 </template>
 
 <script setup lang="ts">
+import { navigate } from 'vike/client/router'
+import { ref } from 'vue'
+
 import SimpleButton from '#components/buttons/SimpleButton.vue'
 import { copyToClipboard } from '#src/utils/copyToClipboard'
 
@@ -35,4 +41,35 @@ const emit = defineEmits<StepEmits>()
 const onClick = (stepId: string) => {
   emit('goTo', stepId)
 }
+
+const buttons = ref([
+  {
+    icon: 'mdi-content-copy',
+    text: 'Link',
+    action: () => copyToClipboard(),
+  },
+  {
+    icon: 'mdi-account-multiple-plus-outline',
+    text: 'Add',
+    action: () => onClick('users'),
+  },
+])
+
+const leaveTable = () => {
+  navigate('/')
+}
 </script>
+
+<style scoped lang="scss">
+.roundButton {
+  color: #3d4753;
+  background-color: #ffffff;
+}
+.roundButton-DarkTheme {
+  color: #f5f5f5;
+  background-color: #8b949b;
+}
+.leaveButton {
+  background-color: #f44336;
+}
+</style>
