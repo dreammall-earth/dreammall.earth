da22e05417ba6494d4fb4b1615e9ab118e7ebdb1
Sebastian Stein <git@codepassion.de>
2024-09-09 16:54:17 +0200

    - updated text resources - fixed lints

--- frontend/src/components/malltalk/settings/TableSettingsRoot.vue
+++ frontend/src/components/malltalk/settings/TableSettingsRoot.vue
@@ -2,7 +2,7 @@
   <div class="flat-text-field d-flex flex-column align-center pa-4 w-100">
     <v-row no-gutters justify="space-around">
       <v-col v-for="(button, index) in buttons" :key="index" cols="auto" class="text-center px-2">
-        <v-btn icon size="x-large" elevation="0" class="roundButton mb-2" @click="button.action">
+        <v-btn icon size="x-large" elevation="0" class="round-button mb-2" @click="button.action">
           <v-icon>{{ button.icon }}</v-icon>
         </v-btn>
         <div class="text-caption">{{ button.text }}</div>
@@ -10,7 +10,7 @@
     </v-row>
     <SimpleButton
       :label="$t('dream-mall-panel.call.leave-table')"
-      class="leaveButton mt-12 mb-5"
+      class="leave-button mt-12 mb-5"
       @click="leaveTable"
     />
   </div>
@@ -19,6 +19,7 @@
 <script setup lang="ts">
 import { navigate } from 'vike/client/router'
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 import SimpleButton from '#components/buttons/SimpleButton.vue'
 import { usePageContext } from '#context/usePageContext'
@@ -28,6 +29,8 @@ import { useTablesStore } from '#stores/tablesStore'
 import { copyToClipboard } from '#src/utils/copyToClipboard'
 
 import type { StepEmits, StepProps } from '#components/steps/StepComponentTypes'
+
+const { t } = useI18n()
 
 defineProps<StepProps>()
 const emit = defineEmits<StepEmits>()
@@ -51,14 +54,14 @@ const showAddAction = computed(() => {
 const buttons = computed(() => [
   {
     icon: 'mdi-content-copy',
-    text: 'Link',
+    text: t('dream-mall-panel.call.link'),
     action: () => copyToClipboard(),
   },
   ...(showAddAction.value
     ? [
         {
           icon: 'mdi-account-multiple-plus-outline',
-          text: 'Add',
+          text: t('dream-mall-panel.call.add-user'),
           action: () => onClick('users'),
         },
       ]
@@ -71,11 +74,12 @@ const leaveTable = () => {
 </script>
 
 <style scoped lang="scss">
-.roundButton {
+.round-button {
   color: rgb(var(--v-theme-dm-panel-call-action-button-color));
   background-color: rgb(var(--v-theme-dm-panel-call-action-button-background-color));
 }
-.leaveButton {
+
+.leave-button {
   background-color: #f44336;
 }
 </style>
