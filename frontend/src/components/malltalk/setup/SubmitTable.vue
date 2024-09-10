348b729bc41288b29c3483c5d0f2236fcaccd44d
Sebastian Stein <git@codepassion.de>
2024-09-10 13:14:35 +0200

    - extended layout with text-field with URL and copy button

--- frontend/src/components/malltalk/setup/SubmitTable.vue
+++ frontend/src/components/malltalk/setup/SubmitTable.vue
@@ -6,11 +6,28 @@
       </p>
     </div>
 
+    <div class="d-flex align-start mt-12">
+      <v-text-field
+        v-model="tableUrl"
+        rounded
+        class="elevation-0 w-100 flex-grow-1 mr-3 custom-text-field"
+        content-class="elevation-0"
+        variant="solo-filled"
+        hide-details
+        readonly
+      />
+      <v-btn icon class="custom-icon-btn" @click="copyUrl">
+        <v-icon icon="mdi-content-copy" />
+      </v-btn>
+    </div>
+
     <SimpleButton class="mt-12 mx-auto" :label="submitText" @click="onSubmit" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
 import SimpleButton from '#components/buttons/SimpleButton.vue'
 
 import type { StepEmits, StepProps } from '#components/steps/StepComponentTypes'
@@ -21,10 +38,32 @@ const emit = defineEmits<StepEmits>()
 const onSubmit = () => {
   emit('submit')
 }
+
+const tableUrl = ref('https.//asdfasdf.de')
+
+const copyUrl = () => {}
 </script>
 
 <style scoped lang="scss">
 .reminder {
   background-color: #8b949b; // todo: save globally
+}
+
+:root {
+  --custom-height: 56px;
+}
+
+.custom-text-field {
+  :deep(.v-field__input) {
+    min-height: var(--custom-height) !important;
+    height: var(--custom-height) !important;
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+}
+
+.custom-icon-btn {
+  height: 48px;
+  width: 48px;
 }
 </style>
