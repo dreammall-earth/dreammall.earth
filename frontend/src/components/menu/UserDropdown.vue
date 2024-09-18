3f38ae38107f09a9f4fffb53f992cd4ea9392ee6
Maximilian Harz <maxharz@gmail.com>
2024-09-18 18:12:01 +0200

    Add light/dark toggle buttons to user menu dropdrown

--- frontend/src/components/menu/UserDropdown.vue
+++ frontend/src/components/menu/UserDropdown.vue
@@ -1,6 +1,12 @@
 <template>
   <ul class="dropdown border-sm pa-3 pb-2 my-2 bg-dropdown-background">
     <li>
+      <button data-test-theme-switch @click="toggleTheme">
+        <v-icon :icon="isDarkTheme ? '$sun' : '$moon'"></v-icon>
+        {{ isDarkTheme ? $t('menu.theme.light') : $t('menu.theme.dark') }}
+      </button>
+    </li>
+    <li>
       <button class="sign-out" @click="signOut">
         <v-icon icon="$logout"></v-icon>{{ $t('buttons.signout') }}
       </button>
@@ -10,9 +16,18 @@
 
 <script lang="ts" setup>
 import { inject } from 'vue'
+import { useTheme } from 'vuetify'
 
 import GlobalErrorHandler from '#plugins/globalErrorHandler'
 import AuthService from '#src/services/AuthService'
+
+const theme = useTheme()
+
+const isDarkTheme = theme.global.current.value.dark
+
+function toggleTheme() {
+  theme.global.name.value = isDarkTheme ? 'light' : 'dark'
+}
 
 const authService = inject<AuthService>('authService')
 
