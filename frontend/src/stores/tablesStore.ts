6f29c01138147f29c579852da2cfd8365f4a7556
elweyn <heine.hannes@gmail.com>
2024-09-19 09:31:50 +0200

    add console log for the subscription error case.

--- frontend/src/stores/tablesStore.ts
+++ frontend/src/stores/tablesStore.ts
@@ -91,23 +91,21 @@ export const useTablesStore = defineStore(
       setTables(data.tables)
     })
 
-    const {
-      result: updateOpenTablesSubscriptionResult /* , error: updateOpenTablesSubscriptionError */,
-    } = useSubscription(
-      updateOpenTablesSubscription,
-      () => ({ username: userStore.getCurrentUser?.username || 'Unknown User' }),
-      { fetchPolicy: 'no-cache' },
-    )
+    const { result: updateOpenTablesSubscriptionResult, error: updateOpenTablesSubscriptionError } =
+      useSubscription(
+        updateOpenTablesSubscription,
+        () => ({ username: userStore.getCurrentUser?.username || 'Unknown User' }),
+        { fetchPolicy: 'no-cache' },
+      )
 
     watch(updateOpenTablesSubscriptionResult, (data: { updateOpenTables: OpenTable[] }) => {
       setOpenTables(data.updateOpenTables)
     })
 
-    /*
     watch(updateOpenTablesSubscriptionError, () => {
+      // eslint-disable-next-line no-console
       console.log(updateOpenTablesSubscriptionError)
     })
-    */
 
     const openTables = ref<OpenTable[]>([])
 
