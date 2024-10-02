3aa82264d8d13441713ffc6018f487e3aabfc74b
Robert Schäfer <git@roschaefer.de>
2024-10-02 17:08:52 +0200

    feat(backend): configure log level
    Motivation
    ----------
    This will allow us to set the global logger's log level with an
    environment variable.
    
    How to test
    -----------
    1. Put e.g. a `logger.trace` somehere
    2. Run the code
    3. No output
    4. `export LOG_LEVEL=SILLY`
    5. Run the code
    6. Log output happens

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -32,7 +32,7 @@ const {
 
   FRONTEND_URL = 'http://localhost:3000/',
 
-  JWKS_URI,
+  JWKS_URI = 'http://localhost:9000/application/o/dreammallearth/jwks/',
 
   WELCOME_TABLE_MEETING_ID = uuidv4(),
   WELCOME_TABLE_NAME = 'DreamMall Coffeetime',
@@ -41,11 +41,9 @@ const {
   SENTRY_ENVIRONMENT,
 
   WEBHOOK_SECRET,
-} = process.env
 
-if (!JWKS_URI) {
-  throw new Error('missing environment variable: JWKS_URI')
-}
+  LOG_LEVEL = 'DEBUG',
+} = process.env
 
 const BREVO = {
   BREVO_KEY,
@@ -81,4 +79,5 @@ export const CONFIG = {
   SENTRY_DSN,
   SENTRY_ENVIRONMENT,
   WEBHOOK_SECRET,
+  LOG_LEVEL,
 }
