3acfe2c22ec8b82dbe2f5358d8ca42e5e79632ae
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-10-09 13:53:53 +0200

    canvas no antialias, powerPreference default

--- frontend/src/components/starmap/StarMap.vue
+++ frontend/src/components/starmap/StarMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="canvas-container">
-    <canvas ref="canvas"></canvas>
+    <canvas ref="canvas" gl="{{powerPreference: 'default', antialias: false}}"></canvas>
   </div>
 </template>
 
