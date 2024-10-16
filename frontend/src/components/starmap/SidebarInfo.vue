c1c10cc20825018d3fe7e71f3f23d064bd9d174e
Maximilian Harz <maxharz@gmail.com>
2024-10-16 16:25:59 +0200

    Show sidebar with infos when clicking stars or show more in hover

---
+++ frontend/src/components/starmap/SidebarInfo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sidebar">
+    <div class="close">
+      <v-btn icon variant="text" size="small" @click="$emit('close')">
+        <v-icon icon="mdi-close" />
+      </v-btn>
+    </div>
+    <v-avatar :size="55" class="avatar mb-2 text-font">
+      <span>{{ getInitials(props.profile.name) }}</span>
+    </v-avatar>
+    <h3>{{ props.profile.name }}</h3>
+    <UserDetails :details="props.profile.details" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import UserDetails from '#components/cockpit/about-me/UserDetails.vue'
+import { getInitials } from '#src/utils/getInitials'
+
+import type { UserWithProfile } from '#stores/userStore'
+
+const props = defineProps<{
+  profile: UserWithProfile
+}>()
+
+defineEmits<{
+  (e: 'close'): void
+}>()
+</script>
+
+<style scoped>
+.sidebar {
+  border-radius: 15px 0px 0px 15px;
+  background: rgba(61, 71, 83, 0.75);
+  backdrop-filter: blur(15px);
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  position: fixed;
+  right: 0;
+  top: 90px;
+  width: min(350px, 100vw);
+  height: 100%;
+  z-index: 200;
+}
+
+.close {
+  display: flex;
+  width: 100%;
+  padding: 8px 16px;
+  place-content: flex-start flex-start;
+  justify-content: flex-end;
+  align-items: flex-start;
+}
+</style>
---
+++ frontend/src/components/starmap/UserDetails.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="details">
+    <li v-for="detail in props.details" :key="detail.id">
+      <v-chip class="detail border-thin">
+        <v-icon
+          :icon="detailCategoryToIcon(detail.category)"
+          class="mr-2"
+          color="cockpit-highlight"
+        ></v-icon>
+        {{ detail.text }}
+      </v-chip>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { detailCategoryToIcon } from '#src/utils/detailCategories'
+
+import type { UserDetail } from '#stores/userStore'
+
+const props = defineProps<{
+  details: UserDetail[]
+}>()
+</script>
+
+<style scoped>
+.detail {
+  background: var(--v-cockpit-chip-background);
+
+  &:deep(.v-chip__underlay) {
+    display: none;
+  }
+}
+
+.details {
+  display: flex;
+  flex-flow: row wrap;
+  gap: 10px;
+  width: 100%;
+  padding: 10px;
+  list-style: none;
+  background: var(--v-cockpit-element-background);
+  border-radius: 15px;
+}
+</style>
