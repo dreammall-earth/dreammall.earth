9a1ea748ed243c1912f85483964c5baaff44c6e7
Robert Schäfer <git@roschaefer.de>
2024-10-18 11:01:13 +0200

    feat(backend): log unexpected brevo api responses (#2731)
    Motivation
    ----------
    So, this should fix: #2680
    
    I was not able to reproduce the error in a manual E2E test. @ulfgebhardt do you know how to reproduce the catch block with Brevo?
    
    I hoped to "unsubscribe" to a newsletter and resubscribe again to get into the catch block, but that would only trigger the "happy path" (status code 2XX).
    
    How to test
    -----------
    1. `cd backend`
    2. `npm run test:unit`
    3. Tests pass

--- backend/src/context/expressContext.ts
+++ backend/src/context/expressContext.ts
@@ -1,13 +1,12 @@
 import { context } from './context'
 
-import type { PrismaClient } from '#src/prisma'
-import type { Context } from './context'
+import type { Context, ContextDependencies } from './context'
 import type { ContextFunction } from '@apollo/server'
 import type { ExpressContextFunctionArgument } from '@apollo/server/express4'
 
-export const expressContext: (deps: {
-  prisma: PrismaClient
-}) => ContextFunction<[ExpressContextFunctionArgument], Context> =
+export const expressContext: (
+  deps: ContextDependencies,
+) => ContextFunction<[ExpressContextFunctionArgument], Context> =
   (deps) =>
   async ({ req }) => {
     const token = req.headers.authorization
