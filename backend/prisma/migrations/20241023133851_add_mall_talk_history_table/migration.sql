8a9cfac0a5ed88b2453840acbfd162371a000b75
Moriz Wahl <moriz.wahl@gmx.de>
2024-10-23 15:39:44 +0200

    add id to mall talk history

--- backend/prisma/migrations/20241023133851_add_mall_talk_history_table/migration.sql
+++ backend/prisma/migrations/20241023133851_add_mall_talk_history_table/migration.sql
@@ -1,11 +1,12 @@
 -- CreateTable
 CREATE TABLE `MallTalkHistory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `fromId` INTEGER NOT NULL,
     `toId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `status` VARCHAR(12) NOT NULL DEFAULT 'UNKNOWN',
 
-    PRIMARY KEY (`fromId`, `toId`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
