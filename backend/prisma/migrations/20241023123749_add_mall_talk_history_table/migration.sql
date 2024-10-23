6b4bf889d381315014301e7be123818e80cf553a
Moriz Wahl <moriz.wahl@gmx.de>
2024-10-23 14:39:31 +0200

    add malltalk history table

---
+++ backend/prisma/migrations/20241023123749_add_mall_talk_history_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `MallTalkHistory` (
+    `fromId` INTEGER NOT NULL,
+    `toId` INTEGER NOT NULL,
+    `createdtA` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status` VARCHAR(12) NOT NULL DEFAULT 'UNKNOWN',
+
+    PRIMARY KEY (`fromId`, `toId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `MallTalkHistory` ADD CONSTRAINT `MallTalkHistory_fromId_fkey` FOREIGN KEY (`fromId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MallTalkHistory` ADD CONSTRAINT `MallTalkHistory_toId_fkey` FOREIGN KEY (`toId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
