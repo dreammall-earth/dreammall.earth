657ca2b8b52a159f60bb7af2265875c47df69435
Maximilian Harz <maxharz@gmail.com>
2024-10-23 18:28:51 +0200

    Get invitation link from backend and show it to user

--- frontend/src/components/invitation-link/InvitationLink.vue
+++ frontend/src/components/invitation-link/InvitationLink.vue
@@ -1,11 +1,28 @@
 <template>
   <MotivationBox>{{ $t('invitationLink.motivation') }}</MotivationBox>
-  <CopyToClipboard :url="myLink"></CopyToClipboard>
+  <CopyToClipboard :url="invitationLink"></CopyToClipboard>
 </template>
 
 <script setup lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import { ref, watch } from 'vue'
+
 import MotivationBox from '#components/cockpit/my-tables/create-table/MotivationBox.vue'
 import CopyToClipboard from '#components/copy-to-clipboard/CopyToClipboard.vue'
+import { createInvitationLinkMutation } from '#mutations/createInvitationLinkMutation'
 
-const myLink = 'foo'
+const invitationLink = ref('')
+
+const { result: createInvitationLinkResult } = useQuery(
+  createInvitationLinkMutation,
+  {},
+  {
+    prefetch: false,
+    fetchPolicy: 'no-cache',
+  },
+)
+
+watch(createInvitationLinkResult, (data: { createInvitationLink: string }) => {
+  invitationLink.value = data.createInvitationLink
+})
 </script>
